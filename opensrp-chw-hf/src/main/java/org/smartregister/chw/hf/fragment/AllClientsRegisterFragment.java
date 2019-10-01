15d8b8e3c22ed2fcbc5071a6764fda31f860854b
Elly Kitoto <junkmailstoelly@gmail.com>
2019-10-01 18:06:45 +0300

    implement all clients register page

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/AllClientsRegisterFragment.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/AllClientsRegisterFragment.java
@@ -1,11 +1,23 @@
 package org.smartregister.chw.hf.fragment;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.opd.fragment.BaseOpdRegisterFragment;
 
-public class AllClientsRegisterFragment  extends BaseOpdRegisterFragment {
+public class AllClientsRegisterFragment extends BaseOpdRegisterFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
     @Override
     protected void startRegistration() {
 
--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/provider/HfAllClientsRegisterProvider.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/provider/HfAllClientsRegisterProvider.java
@@ -2,25 +2,36 @@ package org.smartregister.chw.hf.provider;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
+import org.smartregister.cursoradapter.SmartRegisterQueryBuilder;
 import org.smartregister.opd.configuration.OpdRegisterQueryProviderContract;
 
 public class HfAllClientsRegisterProvider extends OpdRegisterQueryProviderContract {
+
     @NonNull
     @Override
     public String getObjectIdsQuery(@Nullable String filters) {
-        return null;
+        if (TextUtils.isEmpty(filters)) {
+            return "SELECT object_id FROM (SELECT object_id, last_interacted_with FROM ec_child_search WHERE date_removed IS NULL) ORDER BY last_interacted_with";
+        } else {
+            return "SELECT object_id FROM (SELECT object_id, last_interacted_with FROM ec_child_search WHERE date_removed IS NULL AND phrase MATCH '%s*') ORDER BY last_interacted_with";
+        }
     }
 
     @NonNull
     @Override
     public String[] countExecuteQueries(@Nullable String filters) {
-        return new String[0];
+        SmartRegisterQueryBuilder sqb = new SmartRegisterQueryBuilder();
+
+        return new String[]{
+                sqb.countQueryFts("ec_child", null, null, filters),
+        };
     }
 
     @NonNull
     @Override
     public String mainSelectWhereIDsIn() {
-        return null;
+        return "SELECT ec_child.id as _id, ec_child.relational_id as relationalid, ec_child.first_name, ec_child.last_name, ec_child.middle_name, ec_child.gender, ec_child.dob, 'Child' AS register_type FROM ec_child  WHERE is_closed IS 0 AND base_entity_id IN (%s)";
     }
 }
