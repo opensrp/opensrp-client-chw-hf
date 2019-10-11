be82b5e79c996921509f12433d48f72ff8da079b
Elly Kitoto <junkmailstoelly@gmail.com>
2019-10-11 12:23:13 +0300

    change client register rows design

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
@@ -3,6 +3,8 @@ package org.smartregister.chw.hf.fragment;
 import org.smartregister.chw.core.fragment.CoreMalariaRegisterFragment;
 import org.smartregister.chw.core.model.MalariaRegisterFragmentModel;
 import org.smartregister.chw.hf.presenter.MalariaRegisterFragmentPresenter;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.util.Utils;
 import org.smartregister.view.activity.BaseRegisterActivity;
 
 public class MalariaRegisterFragment extends CoreMalariaRegisterFragment {
@@ -16,4 +18,8 @@ public class MalariaRegisterFragment extends CoreMalariaRegisterFragment {
         presenter = new MalariaRegisterFragmentPresenter(this, new MalariaRegisterFragmentModel(), viewConfigurationIdentifier);
     }
 
+    @Override
+    protected void openProfile(CommonPersonObjectClient client) {
+        Utils.showShortToast(getContext(), "Go to malaria profile");
+    }
 }
