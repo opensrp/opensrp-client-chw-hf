b88f6c506587bff45aaa41277302c5c7ec72a9ec
Elly Kitoto <junkmailstoelly@gmail.com>
2019-10-14 12:15:08 +0300

    Implement Malaria profile

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
@@ -2,9 +2,10 @@ package org.smartregister.chw.hf.fragment;
 
 import org.smartregister.chw.core.fragment.CoreMalariaRegisterFragment;
 import org.smartregister.chw.core.model.MalariaRegisterFragmentModel;
+import org.smartregister.chw.hf.activity.MalariaProfileActivity;
 import org.smartregister.chw.hf.presenter.MalariaRegisterFragmentPresenter;
+import org.smartregister.chw.malaria.domain.MemberObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
-import org.smartregister.util.Utils;
 import org.smartregister.view.activity.BaseRegisterActivity;
 
 public class MalariaRegisterFragment extends CoreMalariaRegisterFragment {
@@ -20,6 +21,6 @@ public class MalariaRegisterFragment extends CoreMalariaRegisterFragment {
 
     @Override
     protected void openProfile(CommonPersonObjectClient client) {
-        Utils.showShortToast(getContext(), "Go to malaria profile");
+        MalariaProfileActivity.openProfile(getActivity(), new MemberObject(client), client);
     }
 }
