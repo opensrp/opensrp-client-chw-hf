e619d9c866a222309095bf03cfe330309b616a40
Allan O <a.onchuru@gmail.com>
2020-02-14 14:02:55 +0300

    :fire: Remove unused Upcoming services interactor class

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/FpUpcomingServicesInteractor.java
+++
@@ -1,6 +0,0 @@
-package org.smartregister.chw.hf.interactor;
-
-import org.smartregister.chw.core.interactor.CoreFpUpcomingServicesInteractor;
-
-public class FpUpcomingServicesInteractor extends CoreFpUpcomingServicesInteractor {
-}
