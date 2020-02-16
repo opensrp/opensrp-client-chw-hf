6fda12d4f9c0d567370d7e7b4a750d3ea29215e3
Allan O <a.onchuru@gmail.com>
2020-02-17 00:59:50 +0300

    :recycle: Update HfAllClientsRegisterQuery to get family planning members

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/provider/HfAllClientsRegisterQueryProvider.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/provider/HfAllClientsRegisterQueryProvider.java
@@ -258,6 +258,30 @@ public class HfAllClientsRegisterQueryProvider extends OpdRegisterQueryProviderC
                 "    SELECT ec_child.base_entity_id AS base_entity_id\n" +
                 "    FROM ec_child\n" +
                 ")\n" +
+                "UNION ALL\n" +
+                "\n" +
+                "/*ONLY FAMILY PLANNING PLANNING PATIENTS*/\n" +
+                "SELECT ec_family_member.first_name,\n" +
+                "       ec_family_member.middle_name,\n" +
+                "       ec_family_member.last_name,\n" +
+                "       ec_family_member.gender,\n" +
+                "       ec_family_member.dob,\n" +
+                "       ec_family_member.base_entity_id,\n" +
+                "       ec_family_member.id                          as _id,\n" +
+                "       'Family Planning'                             AS register_type,\n" +
+                "       ec_family_member.relational_id               as relationalid,\n" +
+                "       ec_family.village_town                       as home_address,\n" +
+                "       NULL                                         AS mother_first_name,\n" +
+                "       NULL                                         AS mother_last_name,\n" +
+                "       NULL                                         AS mother_middle_name,\n" +
+                "       ec_family_planning.last_interacted_with AS last_interacted_with\n" +
+                "FROM ec_family_member\n" +
+                "         inner join ec_family on ec_family.base_entity_id = ec_family_member.relational_id\n" +
+                "         inner join ec_family_planning\n" +
+                "                    on ec_family_member.base_entity_id = ec_family_planning.base_entity_id\n" +
+                "where ec_family_member.date_removed is null\n" +
+                "  AND ec_family_planning.is_closed is 0\n" +
+                "  AND ec_family_member.base_entity_id IN (%s)\n" +
                 "ORDER BY last_interacted_with DESC;";
     }
 }
