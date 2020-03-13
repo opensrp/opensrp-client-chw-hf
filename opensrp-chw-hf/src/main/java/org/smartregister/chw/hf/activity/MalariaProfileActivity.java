7e47dfe194473ae2db93d1555d97f3add728e0c5
Elly Kitoto <junkmailstoelly@gmail.com>
2020-03-13 19:17:31 +0300

    Implement malaria referral

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/MalariaProfileActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/MalariaProfileActivity.java
@@ -2,31 +2,54 @@ package org.smartregister.chw.hf.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.core.activity.CoreFamilyProfileActivity;
 import org.smartregister.chw.core.activity.CoreMalariaProfileActivity;
+import org.smartregister.chw.core.model.CoreMalariaRegisterFragmentModel;
 import org.smartregister.chw.core.presenter.CoreFamilyOtherMemberActivityPresenter;
 import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.core.utils.CoreReferralUtils;
 import org.smartregister.chw.hf.R;
+import org.smartregister.chw.hf.adapter.ReferralCardViewAdapter;
+import org.smartregister.chw.hf.contract.MalariaProfileContract;
 import org.smartregister.chw.hf.presenter.FamilyOtherMemberActivityPresenter;
-import org.smartregister.chw.malaria.domain.MemberObject;
-import org.smartregister.chw.malaria.util.Constants;
+import org.smartregister.commonregistry.CommonPersonObject;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.domain.Task;
 import org.smartregister.family.model.BaseFamilyOtherMemberProfileActivityModel;
+import org.smartregister.view.customcontrols.CustomFontTextView;
+
+import java.util.Set;
 
 import timber.log.Timber;
 
+import static org.smartregister.chw.core.utils.CoreReferralUtils.getCommonRepository;
 import static org.smartregister.chw.malaria.util.Constants.ACTIVITY_PAYLOAD.BASE_ENTITY_ID;
 
-public class MalariaProfileActivity extends CoreMalariaProfileActivity {
+public class MalariaProfileActivity extends CoreMalariaProfileActivity implements MalariaProfileContract.InteractorCallback {
+
+    private static boolean isStartedFromReferrals;
+    private static String baseEntityId;
+    public RelativeLayout referralRow;
+    public RecyclerView referralRecyclerView;
+    private CommonPersonObjectClient commonPersonObjectClient;
 
     public static void startMalariaActivity(Activity activity, String baseEntityId) {
+        MalariaProfileActivity.baseEntityId = baseEntityId;
         Intent intent = new Intent(activity, MalariaProfileActivity.class);
         intent.putExtra(BASE_ENTITY_ID, baseEntityId);
+        isStartedFromReferrals = CoreReferralUtils.checkIfStartedFromReferrals(activity);
         activity.startActivity(intent);
     }
 
@@ -52,33 +75,6 @@ public class MalariaProfileActivity extends CoreMalariaProfileActivity {
     }
 
     @Override
-    protected void setupViews() {
-        super.setupViews();
-        rlFamilyServicesDue.setVisibility(View.GONE);
-        rlMalariaPositiveDate.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void setProfileImage(String s, String s1) {
-        //Overridden from abstract class not yet implemented
-    }
-
-    @Override
-    public void setProfileDetailThree(String s) {
-        //Implemented from abstract class but not required right now for HF
-    }
-
-    @Override
-    public void toggleFamilyHead(boolean b) {
-        //Implemented from abstract class but not required right now for HF
-    }
-
-    @Override
-    public void togglePrimaryCaregiver(boolean b) {
-        //Implemented from abstract class but not required right now for HF
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         MenuItem item = menu.findItem(R.id.action_remove_member);
@@ -101,11 +97,70 @@ public class MalariaProfileActivity extends CoreMalariaProfileActivity {
     @NonNull
     @Override
     public CoreFamilyOtherMemberActivityPresenter presenter() {
-        MemberObject memberObject = (MemberObject) getIntent().getSerializableExtra(Constants.MALARIA_MEMBER_OBJECT.MEMBER_OBJECT);
-        return new FamilyOtherMemberActivityPresenter(this, new BaseFamilyOtherMemberProfileActivityModel(),
-                null, memberObject.getRelationalId(), memberObject.getBaseEntityId(),
-                memberObject.getFamilyHead(), memberObject.getPrimaryCareGiver(), memberObject.getAddress(),
-                memberObject.getLastName());
+        if (presenter == null) {
+            presenter = new FamilyOtherMemberActivityPresenter(this, new BaseFamilyOtherMemberProfileActivityModel(),
+                    null, memberObject.getRelationalId(), memberObject.getBaseEntityId(),
+                    memberObject.getFamilyHead(), memberObject.getPrimaryCareGiver(), memberObject.getAddress(),
+                    memberObject.getLastName());
+        }
+        return (CoreFamilyOtherMemberActivityPresenter) presenter;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        updateCommonPersonObjectClient();
+        initializeTasksRecyclerView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        super.onResumption();
+        updateTitleWhenFromReferrals();
+        ((FamilyOtherMemberActivityPresenter) presenter()).getReferralTasks(CoreConstants.REFERRAL_PLAN_ID, baseEntityId, this);
+        if (referralRecyclerView != null && referralRecyclerView.getAdapter() != null) {
+            referralRecyclerView.getAdapter().notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    protected void setupViews() {
+        super.setupViews();
+        initializeTasksRecyclerView();
+    }
+
+    private void updateTitleWhenFromReferrals() {
+        if (isStartedFromReferrals) {
+            ((CustomFontTextView) findViewById(R.id.toolbar_title)).setText(getString(R.string.return_to_task_details));
+        }
+    }
+
+    private void initializeTasksRecyclerView() {
+        referralRecyclerView = findViewById(R.id.referral_card_recycler_view);
+        referralRow = findViewById(R.id.referral_row);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        referralRecyclerView.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    public void setProfileImage(String s, String s1) {
+        //Overridden from abstract class not yet implemented
+    }
+
+    @Override
+    public void setProfileDetailThree(String s) {
+        //Implemented from abstract class but not required right now for HF
+    }
+
+    @Override
+    public void toggleFamilyHead(boolean b) {
+        //Implemented from abstract class but not required right now for HF
+    }
+
+    @Override
+    public void togglePrimaryCaregiver(boolean b) {
+        //Implemented from abstract class but not required right now for HF
     }
 
     @Override
@@ -131,5 +186,41 @@ public class MalariaProfileActivity extends CoreMalariaProfileActivity {
     @Override
     public void notifyHasPhone(boolean hasPhone) {
         //Overridden from abstract class not yet implemented
+    }
+
+    @Override
+    public void updateReferralTasks(Set<Task> taskList) {
+        if (referralRecyclerView != null && taskList.size() > 0) {
+            RecyclerView.Adapter mAdapter = new ReferralCardViewAdapter(taskList, this, getAncMemberObject(), memberObject.getFamilyHeadName(),
+                    memberObject.getFamilyHeadPhoneNumber(), commonPersonObjectClient, CoreConstants.REGISTERED_ACTIVITIES.MALARIA_REGISTER_ACTIVITY);
+            referralRecyclerView.setAdapter(mAdapter);
+            referralRow.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public MemberObject getAncMemberObject() {
+        MemberObject memberObject = new MemberObject();
+        memberObject.setFirstName(this.memberObject.getFirstName());
+        memberObject.setBaseEntityId(this.memberObject.getBaseEntityId());
+        memberObject.setFirstName(this.memberObject.getFirstName());
+        memberObject.setLastName(this.memberObject.getLastName());
+        memberObject.setMiddleName(this.memberObject.getMiddleName());
+        memberObject.setDob(this.memberObject.getAge());
+        return memberObject;
+    }
+
+    public void updateCommonPersonObjectClient() {
+        String query = new CoreMalariaRegisterFragmentModel().mainSelect(CoreConstants.TABLE_NAME.MALARIA_CONFIRMATION, String.format("ec_malaria_confirmation.base_entity_id = '%s'", baseEntityId));
+
+        try (Cursor cursor = getCommonRepository(CoreConstants.TABLE_NAME.MALARIA_CONFIRMATION).rawCustomQueryForAdapter(query)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                CommonPersonObject personObject = getCommonRepository(CoreConstants.TABLE_NAME.MALARIA_CONFIRMATION).readAllcommonforCursorAdapter(cursor);
+                commonPersonObjectClient = new CommonPersonObjectClient(personObject.getCaseId(),
+                        personObject.getDetails(), "");
+                commonPersonObjectClient.setColumnmaps(personObject.getColumnmaps());
+            }
+        } catch (Exception ex) {
+            Timber.e(ex, "Malaria profile --> setCommonPersonObjectClient");
+        }
     }
 }
--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
@@ -2,11 +2,25 @@ package org.smartregister.chw.hf.fragment;
 
 import org.smartregister.chw.core.fragment.CoreMalariaRegisterFragment;
 import org.smartregister.chw.core.model.CoreMalariaRegisterFragmentModel;
+import org.smartregister.chw.core.provider.ChwMalariaRegisterProvider;
 import org.smartregister.chw.hf.activity.MalariaProfileActivity;
 import org.smartregister.chw.hf.presenter.MalariaRegisterFragmentPresenter;
+import org.smartregister.chw.hf.provider.HfMalariaRegisterProvider;
+import org.smartregister.configurableviews.model.View;
+import org.smartregister.cursoradapter.RecyclerViewPaginatedAdapter;
 import org.smartregister.view.activity.BaseRegisterActivity;
 
+import java.util.Set;
+
 public class MalariaRegisterFragment extends CoreMalariaRegisterFragment {
+
+    @Override
+    public void initializeAdapter(Set<View> visibleColumns) {
+        ChwMalariaRegisterProvider malariaRegisterProvider = new HfMalariaRegisterProvider(getActivity(), paginationViewHandler, registerActionHandler, visibleColumns);
+        clientAdapter = new RecyclerViewPaginatedAdapter(null, malariaRegisterProvider, context().commonrepository(this.tablename));
+        clientAdapter.setCurrentlimit(20);
+        clientsView.setAdapter(clientAdapter);
+    }
 
     @Override
     protected void initializePresenter() {
