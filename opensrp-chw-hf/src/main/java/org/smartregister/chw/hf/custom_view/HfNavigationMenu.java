4b2365013568696265793d0bb16217fb78bf1257
Allan O <a.onchuru@gmail.com>
2020-03-31 18:27:19 +0300

    :fire: Implement NavigationMenu overrides

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/custom_view/HfNavigationMenu.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/custom_view/HfNavigationMenu.java
@@ -5,7 +5,6 @@ import org.smartregister.chw.core.custom_views.NavigationMenu;
 import org.smartregister.chw.core.utils.CoreConstants;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -14,23 +13,7 @@ public class HfNavigationMenu implements NavigationMenu.Flavour {
 
     @Override
     public List<Pair<String, Locale>> getSupportedLanguages() {
-
-        return Collections.singletonList(new Pair<String, Locale>() {
-            @Override
-            public String getLeft() {
-                return "English";
-            }
-
-            @Override
-            public Locale getRight() {
-                return Locale.ENGLISH;
-            }
-
-            @Override
-            public Locale setValue(Locale locale) {
-                return null;
-            }
-        });
+        return Arrays.asList(Pair.of("English", Locale.ENGLISH), Pair.of("Kiswahili", new Locale("sw")));
     }
 
     @Override
