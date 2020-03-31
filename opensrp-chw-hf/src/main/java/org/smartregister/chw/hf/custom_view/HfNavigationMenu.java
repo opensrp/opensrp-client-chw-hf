cc5e40079141150fe8501772ea9c724f4fa09d0e
Allan O <a.onchuru@gmail.com>
2020-03-31 18:20:57 +0300

    :arrow_up: Update chw-core snapshot

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/custom_view/HfNavigationMenu.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/custom_view/HfNavigationMenu.java
@@ -1,14 +1,36 @@
 package org.smartregister.chw.hf.custom_view;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.smartregister.chw.core.custom_views.NavigationMenu;
 import org.smartregister.chw.core.utils.CoreConstants;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 public class HfNavigationMenu implements NavigationMenu.Flavour {
+
     @Override
-    public String[] getSupportedLanguages() {
-        return new String[]{"English", "Kiswahili"};
+    public List<Pair<String, Locale>> getSupportedLanguages() {
+
+        return Collections.singletonList(new Pair<String, Locale>() {
+            @Override
+            public String getLeft() {
+                return "English";
+            }
+
+            @Override
+            public Locale getRight() {
+                return Locale.ENGLISH;
+            }
+
+            @Override
+            public Locale setValue(Locale locale) {
+                return null;
+            }
+        });
     }
 
     @Override
@@ -16,5 +38,15 @@ public class HfNavigationMenu implements NavigationMenu.Flavour {
         HashMap<String, String> tableMap = new HashMap<>();
         tableMap.put(CoreConstants.DrawerMenu.REFERRALS, CoreConstants.TABLE_NAME.TASK);
         return tableMap;
+    }
+
+    @Override
+    public boolean hasServiceReport() {
+        return false;
+    }
+
+    @Override
+    public boolean hasStockReport() {
+        return false;
     }
 }
