454e5bef26cb6993620d797a907c99b4cc9bfd02
paulinembabu <pauline.mbabu@gmail.com>
2020-04-07 18:24:19 +0300

    Adds Implementation on HFNavigationMenu

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/custom_view/HfNavigationMenu.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/custom_view/HfNavigationMenu.java
@@ -1,8 +1,15 @@
 package org.smartregister.chw.hf.custom_view;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
 import org.apache.commons.lang3.tuple.Pair;
+import org.smartregister.chw.core.activity.HIA2ReportsActivity;
 import org.smartregister.chw.core.custom_views.NavigationMenu;
 import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.hf.R;
+import org.smartregister.chw.hf.adapter.ProviderStockUsageReportListAdapter;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -10,11 +17,13 @@ import java.util.List;
 import java.util.Locale;
 
 public class HfNavigationMenu implements NavigationMenu.Flavour {
+    private View rootView = null;
 
     @Override
     public List<Pair<String, Locale>> getSupportedLanguages() {
         return Arrays.asList(Pair.of("English", Locale.ENGLISH), Pair.of("Kiswahili", new Locale("sw")));
     }
+
     @Override
     public HashMap<String, String> getTableMapValues() {
         HashMap<String, String> tableMap = new HashMap<>();
@@ -23,12 +32,28 @@ public class HfNavigationMenu implements NavigationMenu.Flavour {
     }
 
     @Override
-    public boolean hasServiceReport() {
-        return false;
+    public void registerServiceActivity(Activity activity) {
+        View rlIconServiceReport = rootView.findViewById(R.id.rlServiceReport);
+        rlIconServiceReport.setVisibility(View.VISIBLE);
+        rlIconServiceReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, HIA2ReportsActivity.class);
+                activity.startActivity(intent);
+            }
+        });
     }
 
     @Override
-    public boolean hasStockReport() {
-        return false;
+    public void registerStockReport(Activity activity) {
+        View rlIconStockReport = rootView.findViewById(R.id.rlIconStockReport);
+        rlIconStockReport.setVisibility(View.VISIBLE);
+        rlIconStockReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, ProviderStockUsageReportListAdapter.class);
+                activity.startActivity(intent);
+            }
+        });
     }
 }
