1df867b357adeb7e8b24edc40fc11cf2b911159a
Allan O <a.onchuru@gmail.com>
2020-05-15 13:23:14 +0300

    :construction: Save FP start/change as DateTime

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/HFFamilyPlanningUtil.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/HFFamilyPlanningUtil.java
@@ -1,5 +1,11 @@
 package org.smartregister.chw.hf.utils;
 
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+
+import org.jetbrains.annotations.NotNull;
+import org.joda.time.LocalDateTime;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.chw.anc.util.NCUtils;
 import org.smartregister.chw.core.dao.ChwNotificationDao;
@@ -25,9 +31,11 @@ public class HFFamilyPlanningUtil extends FpUtil {
 
     public static void createFamilyPlanningUpdateEvent(String jsonString, String baseEntityId) {
         try {
-            // Set the FP update event type
+            // Set the FP update event type and update time for fp_reg
             JSONObject formObject = new JSONObject(jsonString);
+            String currentEncounter = formObject.getString(JsonFormUtils.ENCOUNTER_TYPE);
             formObject.put(JsonFormUtils.ENCOUNTER_TYPE, CoreConstants.EventType.FAMILY_PLANNING_UPDATE);
+            populateStartDateTime(formObject, currentEncounter);
             AllSharedPreferences allSharedPreferences = Utils.getAllSharedPreferences();
             Event baseEvent = org.smartregister.chw.anc.util.JsonFormUtils.processJsonForm(allSharedPreferences, CoreReferralUtils.setEntityId(formObject.toString(), baseEntityId), CoreConstants.TABLE_NAME.FAMILY_PLANNING_UPDATE);
             org.smartregister.chw.anc.util.JsonFormUtils.tagEvent(allSharedPreferences, baseEvent);
@@ -41,5 +49,18 @@ public class HFFamilyPlanningUtil extends FpUtil {
         } catch (Exception ex) {
             Timber.e(ex);
         }
+    }
+
+    public static void populateStartDateTime(@NotNull JSONObject jsonObject, String encounter) throws JSONException {
+        String dateTimeString = new LocalDateTime().toString("yyyy-MM-dd HH:mm:ss");
+        JSONObject jsonStepObject = jsonObject.getJSONObject("step1");
+        JSONArray fieldsArray = jsonStepObject.getJSONArray(JsonFormConstants.FIELDS);
+        String key = "fp_reg_date";
+        if (encounter.equals("Family Planning Change Method")) {
+            key = "fp_change_or_stop_date";
+        }
+        JSONObject startDateObject = JsonFormUtils.getFieldJSONObject(fieldsArray, key);
+        if (startDateObject != null)
+            startDateObject.put(JsonFormUtils.VALUE, dateTimeString);
     }
 }
