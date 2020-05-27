9b6368f3867f12900c81e3c313c3c462794b1bd0
Allan O <a.onchuru@gmail.com>
2020-05-27 18:05:34 +0300

    :card_file_box: Add migrations to add syncLocationId column and IDs

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/dao/FamilyDao.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/dao/FamilyDao.java
@@ -1,9 +1,16 @@
 package org.smartregister.chw.hf.dao;
 
+import android.util.Pair;
+
+import org.json.JSONObject;
 import org.smartregister.chw.hf.model.FamilyDetailsModel;
 import org.smartregister.dao.AbstractDao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import timber.log.Timber;
 
 public class FamilyDao extends AbstractDao {
 
@@ -29,5 +36,38 @@ public class FamilyDao extends AbstractDao {
             return null;
 
         return familyProfileModels.get(0);
+    }
+
+    public static void migrateAddLocationIdCol() {
+        String updateSqlString = "ALTER TABLE ec_family_member ADD COLUMN sync_location_id";
+        updateDB(updateSqlString);
+    }
+
+    public static void migrateInsertLocationIDs() {
+        String getLocationIdSQL = "select event.json, ec_family_member.base_entity_id \n" +
+                "from event \n" +
+                "inner join ec_family_member on event.baseEntityId = ec_family_member.base_entity_id\n" +
+                "where event.eventType = 'Family Member Registration' and ec_family_member.is_closed = '0'";
+
+        DataMap<Pair<String, String>> dataMap = cursor -> new Pair<>(getLocationId(getCursorValue(cursor, "json")), getCursorValue(cursor, "base_entity_id"));
+        List<Pair<String, String>> jsonPairs = readData(getLocationIdSQL, dataMap);
+        String locationId;
+        String baseEntityId;
+        String updateSQLString = "UPDATE ec_family_member SET sync_location_id = '%s' WHERE base_entity_id = '%s'";
+        for (Pair<String, String> locationIDEntityPair : jsonPairs) {
+            locationId = locationIDEntityPair.first;
+            baseEntityId = locationIDEntityPair.second;
+            updateDB(String.format(updateSQLString, locationId, baseEntityId));
+        }
+    }
+
+    public static String getLocationId(String jsonString) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            return jsonObject.getString("locationId");
+        } catch (Exception ex) {
+            Timber.e(ex);
+            return "";
+        }
     }
 }
