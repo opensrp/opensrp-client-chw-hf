45b7e41e572dc75a5fd2dc42e06aac311bdb1e3b
cozej4 <ilakozejumanne@gmail.com>
2021-06-12 14:53:16 +0300

    Refactored hiv index clients registration form fixed issues on saving dob

---
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/presenter/HivIndexRegisterFragmentPresenter.java
@@ -0,0 +1,44 @@
+package org.smartregister.chw.hf.presenter;
+
+import org.jetbrains.annotations.NotNull;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.hf.R;
+import org.smartregister.chw.hiv.contract.BaseHivRegisterFragmentContract;
+import org.smartregister.chw.hiv.presenter.BaseHivRegisterFragmentPresenter;
+import org.smartregister.chw.hiv.util.Constants.Tables;
+import org.smartregister.chw.hiv.util.DBConstants;
+
+public class HivRegisterFragmentPresenter extends BaseHivRegisterFragmentPresenter {
+
+    public HivRegisterFragmentPresenter(BaseHivRegisterFragmentContract.View view, BaseHivRegisterFragmentContract.Model model, String viewConfigurationIdentifier) {
+        super(view, model, viewConfigurationIdentifier);
+    }
+
+    @Override
+    @NotNull
+    public String getMainCondition() {
+        return " " + CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.Key.DATE_REMOVED + " is null " +
+                "AND (" + Tables.HIV + "." + DBConstants.Key.CLIENT_HIV_STATUS_AFTER_TESTING + " = 'Positive' OR " + Tables.HIV + "." + DBConstants.Key.CLIENT_HIV_STATUS_AFTER_TESTING + " IS NULL ) "+
+                "AND " + Tables.HIV + "." + DBConstants.Key.IS_CLOSED + " = '0' ";
+
+    }
+
+    @Override
+    @NotNull
+    public String getDueFilterCondition() {
+        return CoreConstants.TABLE_NAME.HIV_MEMBER + ".base_entity_id IN (SELECT for FROM task WHERE business_status = 'Referred')";
+    }
+
+    @Override
+    public void processViewConfigurations() {
+        super.processViewConfigurations();
+        if (getConfig().getSearchBarText() != null && getView() != null) {
+            getView().updateSearchBarHint(getView().getContext().getString(R.string.search_name_or_id));
+        }
+    }
+
+    @Override
+    public String getMainTable() {
+        return Tables.HIV;
+    }
+}
