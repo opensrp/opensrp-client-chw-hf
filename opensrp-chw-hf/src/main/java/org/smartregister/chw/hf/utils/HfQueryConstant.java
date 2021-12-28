f61125b217eb55ae0ea7fd33fa6c4828ad447491
Billy <billypatrick767@gmail.com>
2021-12-28 12:11:42 +0300

    updated: query constant to factor in closed clients in anc and pnc

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/HfQueryConstant.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/HfQueryConstant.java
@@ -71,10 +71,10 @@ public interface HfQueryConstant {
             "  AND ec_hiv_register.base_entity_id IN (%s)\n" +
             "  AND ec_family_member.base_entity_id NOT IN (\n" +
             "    SELECT ec_anc_register.base_entity_id AS base_entity_id\n" +
-            "    FROM ec_anc_register\n" +
+            "    FROM ec_anc_register where ec_anc_register.is_closed is 0\n" +
             "    UNION ALL\n" +
             "    SELECT ec_pregnancy_outcome.base_entity_id AS base_entity_id\n" +
-            "    FROM ec_pregnancy_outcome\n" +
+            "    FROM ec_pregnancy_outcome where ec_pregnancy_outcome.delivery_date is not null\n" +
             "    UNION ALL\n" +
             "    SELECT ec_child.base_entity_id AS base_entity_id\n" +
             "    FROM ec_child\n" +
@@ -151,10 +151,10 @@ public interface HfQueryConstant {
             "  AND ec_family_member.base_entity_id IN (%s)\n" +
             "  AND ec_family_member.base_entity_id NOT IN (\n" +
             "    SELECT ec_anc_register.base_entity_id AS base_entity_id\n" +
-            "    FROM ec_anc_register\n" +
+            "    FROM ec_anc_register where ec_anc_register.is_closed is 0\n" +
             "    UNION ALL\n" +
             "    SELECT ec_pregnancy_outcome.base_entity_id AS base_entity_id\n" +
-            "    FROM ec_pregnancy_outcome\n" +
+            "    FROM ec_pregnancy_outcome where ec_pregnancy_outcome.delivery_date is not null\n" +
             "    UNION ALL\n" +
             "    SELECT ec_child.base_entity_id AS base_entity_id\n" +
             "    FROM ec_child\n" +
