f14033f0c0bfc028f863af8d8d0eaf76c4de3402
Billy <billypatrick767@gmail.com>
2022-03-10 18:41:28 +0300

    fixed visit processing for pmtct

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/PmtctVisitUtils.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/PmtctVisitUtils.java
@@ -3,6 +3,7 @@ package org.smartregister.chw.hf.utils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.smartregister.chw.hf.dao.HfPmtctDao;
 import org.smartregister.chw.pmtct.PmtctLibrary;
 import org.smartregister.chw.pmtct.domain.Visit;
 import org.smartregister.chw.pmtct.repository.VisitDetailsRepository;
@@ -16,9 +17,10 @@ import java.util.List;
 import timber.log.Timber;
 
 public class PmtctVisitUtils extends VisitUtils {
-    public static void processVisits() throws Exception{
+    public static void processVisits() throws Exception {
         processVisits(PmtctLibrary.getInstance().visitRepository(), PmtctLibrary.getInstance().visitDetailsRepository());
     }
+
     public static void processVisits(VisitRepository visitRepository, VisitDetailsRepository visitDetailsRepository) throws Exception {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.HOUR_OF_DAY, 0);
@@ -28,8 +30,8 @@ public class PmtctVisitUtils extends VisitUtils {
         List<Visit> pmtctFollowupVisits = new ArrayList<>();
 
 
-        for(Visit v: visits){
-            if(v.getVisitType().equalsIgnoreCase(Constants.Events.PMTCT_FIRST_EAC_VISIT)){
+        for (Visit v : visits) {
+            if (v.getVisitType().equalsIgnoreCase(Constants.Events.PMTCT_FIRST_EAC_VISIT)) {
                 try {
                     JSONObject jsonObject = new JSONObject(v.getJson());
                     JSONArray obs = jsonObject.getJSONArray("obs");
@@ -39,45 +41,82 @@ public class PmtctVisitUtils extends VisitUtils {
                     boolean isEacVisit3Done = computeCompletionStatus(obs, "eac_day_3");
 
 
-                    if(isEacVisit1Done && isEacVisit2Done && isEacVisit3Done){
+                    if (isEacVisit1Done && isEacVisit2Done && isEacVisit3Done) {
                         pmtctEacVisitsCompleted.add(v);
                     }
-                } catch (Exception e){
+                } catch (Exception e) {
                     Timber.e(e);
                 }
-            }
-            else if(v.getVisitType().equalsIgnoreCase(Constants.Events.PMTCT_SECOND_EAC_VISIT)){
-                try{
+            } else if (v.getVisitType().equalsIgnoreCase(Constants.Events.PMTCT_SECOND_EAC_VISIT)) {
+                try {
                     JSONObject jsonObject = new JSONObject(v.getJson());
                     JSONArray obs = jsonObject.getJSONArray("obs");
 
-                    boolean isEacVisit1Done = computeCompletionStatus(obs,"eac_month_1");
-                    boolean isEacVisit2Done = computeCompletionStatus(obs,"eac_month_2");
-                    boolean isEacVisit3Done = computeCompletionStatus(obs,"eac_month_3");
+                    boolean isEacVisit1Done = computeCompletionStatus(obs, "eac_month_1");
+                    boolean isEacVisit2Done = computeCompletionStatus(obs, "eac_month_2");
+                    boolean isEacVisit3Done = computeCompletionStatus(obs, "eac_month_3");
 
-                    if(isEacVisit1Done && isEacVisit2Done && isEacVisit3Done){
+                    if (isEacVisit1Done && isEacVisit2Done && isEacVisit3Done) {
                         pmtctEacVisitsCompleted.add(v);
                     }
-                } catch (Exception e){
+                } catch (Exception e) {
                     Timber.e(e);
                 }
-            }else if(v.getVisitType().equalsIgnoreCase(org.smartregister.chw.pmtct.util.Constants.EVENT_TYPE.PMTCT_FOLLOWUP)){
-                pmtctFollowupVisits.add(v);
+            } else if (v.getVisitType().equalsIgnoreCase(org.smartregister.chw.pmtct.util.Constants.EVENT_TYPE.PMTCT_FOLLOWUP)) {
+                try {
+                    JSONObject jsonObject = new JSONObject(v.getJson());
+                    String baseEntityId = jsonObject.getString("baseEntityId");
+                    JSONArray obs = jsonObject.getJSONArray("obs");
+                    List<Boolean> checks = new ArrayList<Boolean>();
+
+                    boolean isCounsellingDone = computeCompletionStatus(obs, "is_client_counselled");
+                    boolean isClinicalStagingDone = computeCompletionStatus(obs, "clinical_staging_disease");
+                    boolean isTbScreeningDone = computeCompletionStatus(obs, "on_tb_treatment");
+                    boolean isArvPrescriptionDone = computeCompletionStatus(obs, "prescribed_regimes");
+
+                    boolean isBaselineInvestigationComplete = computeCompletionStatus(obs, "liver_function_test_conducted") && computeCompletionStatus(obs, "renal_function_test_conducted");
+                    boolean isHvlSampleCollectionComplete = computeCompletionStatus(obs, "hvl_sample_id");
+                    boolean isCd4SampleCollectionComplete = computeCompletionStatus(obs, "cd4_sample_id");
+
+                    checks.add(isCounsellingDone);
+                    checks.add(isClinicalStagingDone);
+                    checks.add(isTbScreeningDone);
+                    checks.add(isArvPrescriptionDone);
+
+
+                    if (HfPmtctDao.isEligibleForHlvTest(baseEntityId)) {
+                        checks.add(isHvlSampleCollectionComplete);
+                    }
+
+                    if (HfPmtctDao.isEligibleForBaselineInvestigation(baseEntityId) || HfPmtctDao.isEligibleForBaselineInvestigationOnFollowupVisit(baseEntityId)) {
+                        checks.add(isBaselineInvestigationComplete);
+                    }
+
+                    if (HfPmtctDao.isEligibleForCD4Retest(baseEntityId) || HfPmtctDao.isEligibleForCD4Test(baseEntityId)) {
+                        checks.add(isCd4SampleCollectionComplete);
+                    }
+
+                    if (!checks.contains(false)) {
+                        pmtctEacVisitsCompleted.add(v);
+                    }
+                } catch (Exception e) {
+                    Timber.e(e);
+                }
             }
         }
-        if(pmtctEacVisitsCompleted.size() > 0){
-            processVisits(pmtctEacVisitsCompleted,visitRepository,visitDetailsRepository);
+        if (pmtctEacVisitsCompleted.size() > 0) {
+            processVisits(pmtctEacVisitsCompleted, visitRepository, visitDetailsRepository);
         }
-        if(pmtctFollowupVisits.size() > 0){
-            processVisits(pmtctFollowupVisits,visitRepository,visitDetailsRepository);
+        if (pmtctFollowupVisits.size() > 0) {
+            processVisits(pmtctFollowupVisits, visitRepository, visitDetailsRepository);
         }
     }
 
     public static boolean computeCompletionStatus(JSONArray obs, String checkString) throws JSONException {
         int size = obs.length();
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             JSONObject checkObj = obs.getJSONObject(i);
-            if(checkObj.getString("fieldCode").equalsIgnoreCase(checkString)){
+            if (checkObj.getString("fieldCode").equalsIgnoreCase(checkString)) {
                 return true;
             }
         }
