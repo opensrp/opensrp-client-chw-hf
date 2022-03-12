0c3080d8f2a0a83a7cc8985d2b93fe7101cf7201
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-03-12 10:56:50 +0300

    Refactored hfPMTCT Dao

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/dao/HfPmtctDao.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/dao/HfPmtctDao.java
@@ -45,6 +45,12 @@ public class HfPmtctDao extends CorePmtctDao {
             return eligible;
         }
 
+
+        eligible = isEligibleForHlvTestForClientsWithPreviousLackOfSuppression(baseEntityID);
+        if (eligible != null) {
+            return eligible;
+        }
+
         eligible = isEligibleForHlvTestForClientsWithPreviousHvlTests(baseEntityID);
         if (eligible != null) {
             return eligible;
@@ -72,6 +78,28 @@ public class HfPmtctDao extends CorePmtctDao {
 
         DataMap<String> registrationDateMap = cursor -> getCursorValue(cursor, "pmtct_register_date");
         List<String> res = readData(sql, registrationDateMap);
+        if (res != null && res.size() > 0 && res.get(0) != null) {
+            return getElapsedTimeInMonths(res.get(0)) >= 3;
+        }
+        return null;
+    }
+
+    public static Boolean isEligibleForHlvTestForClientsWithPreviousLackOfSuppression(String baseEntityID) {
+        //Checking eligibility for  PMTCT clients with lack of suppression after EAC visits
+        String sql = "SELECT hvl_collection_date\n" +
+                "FROM (SELECT *\n" +
+                "      FROM ec_pmtct_followup\n" +
+                "      WHERE entity_id = '" + baseEntityID + "'\n" +
+                "        AND hvl_sample_id IS NOT NULL\n" +
+                "        AND hvl_collection_date IS NOT NULL\n" +
+                "      ORDER BY visit_number DESC\n" +
+                "      LIMIT 1) pm\n" +
+                "         INNER JOIN ec_pmtct_hvl_results ephr on pm.base_entity_id = ephr.hvl_pmtct_followup_form_submission_id\n" +
+                "WHERE ephr.hvl_result > 1000 AND ephr.hvl_result IS NOT NULL";
+
+
+        DataMap<String> hvlCollectionDateMap = cursor -> getCursorValue(cursor, "hvl_collection_date");
+        List<String> res = readData(sql, hvlCollectionDateMap);
         if (res != null && res.size() > 0 && res.get(0) != null) {
             return getElapsedTimeInMonths(res.get(0)) >= 3;
         }
