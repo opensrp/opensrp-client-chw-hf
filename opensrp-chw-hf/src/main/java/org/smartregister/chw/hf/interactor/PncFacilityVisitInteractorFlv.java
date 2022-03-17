0a5ea3c9d0de42f48eea2ac862e18b9a939f8743
Billy <billypatrick767@gmail.com>
2022-03-17 11:13:53 +0300

    code fixes

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/PncFacilityVisitInteractorFlv.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/PncFacilityVisitInteractorFlv.java
@@ -1,17 +1,10 @@
 package org.smartregister.chw.hf.interactor;
 
-import org.smartregister.chw.anc.AncLibrary;
 import org.smartregister.chw.anc.contract.BaseAncHomeVisitContract;
 import org.smartregister.chw.anc.domain.MemberObject;
-import org.smartregister.chw.anc.domain.Visit;
-import org.smartregister.chw.anc.domain.VisitDetail;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
-import org.smartregister.chw.anc.util.VisitUtils;
-import org.smartregister.chw.hf.utils.Constants;
 
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 public class PncFacilityVisitInteractorFlv implements AncFirstFacilityVisitInteractor.Flavor {
     LinkedHashMap<String, BaseAncHomeVisitAction> actionList = new LinkedHashMap<>();
@@ -19,14 +12,17 @@ public class PncFacilityVisitInteractorFlv implements AncFirstFacilityVisitInter
 
     @Override
     public LinkedHashMap<String, BaseAncHomeVisitAction> calculateActions(BaseAncHomeVisitContract.View view, MemberObject memberObject, BaseAncHomeVisitContract.InteractorCallBack callBack) throws BaseAncHomeVisitAction.ValidationException {
-        Map<String, List<VisitDetail>> details = null;
-        // get the preloaded data
-        if (view.getEditMode()) {
-            Visit lastVisit = AncLibrary.getInstance().visitRepository().getLatestVisit(memberObject.getBaseEntityId(), Constants.Events.PNC_VISIT);
-            if (lastVisit != null) {
-                details = VisitUtils.getVisitGroups(AncLibrary.getInstance().visitDetailsRepository().getVisits(lastVisit.getVisitId()));
-            }
-        }
+
+        //TODO uncomment to get details
+
+//        Map<String, List<VisitDetail>> details = null;
+//        // get the preloaded data
+//        if (view.getEditMode()) {
+//            Visit lastVisit = AncLibrary.getInstance().visitRepository().getLatestVisit(memberObject.getBaseEntityId(), Constants.Events.PNC_VISIT);
+//            if (lastVisit != null) {
+//                details = VisitUtils.getVisitGroups(AncLibrary.getInstance().visitDetailsRepository().getVisits(lastVisit.getVisitId()));
+//            }
+//        }
 
         // TODO implement actions for PNC
         //evaluatePncActions(view, memberObject, callBack, details);
