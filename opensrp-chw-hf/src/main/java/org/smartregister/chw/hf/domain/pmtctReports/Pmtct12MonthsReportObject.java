0e6cae0afeca9a9d4a1c15008d6847fc669d94ba
Billy <billypatrick767@gmail.com>
2022-03-26 13:33:51 +0300

    cleaned: how data is loaded for reports

---
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/domain/pmtctReports/Pmtct12MonthsReportObject.java
@@ -0,0 +1,76 @@
+package org.smartregister.chw.hf.domain.pmtctReports;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.chw.hf.dao.PmtctReportDao;
+import org.smartregister.chw.hf.domain.ReportObject;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Pmtct12MonthsReportObject extends ReportObject {
+    DecimalFormat df = new DecimalFormat();
+    ;
+    private List<String> indicatorCodes = new ArrayList<>();
+    private Date reportDate;
+
+    public Pmtct12MonthsReportObject(Date reportDate) {
+        super(reportDate);
+        this.reportDate = reportDate;
+        setIndicatorCodes(indicatorCodes);
+    }
+
+    public void setIndicatorCodes(List<String> indicatorCodes) {
+        indicatorCodes.add("A12");
+        indicatorCodes.add("B12");
+        indicatorCodes.add("C12");
+        indicatorCodes.add("D12");
+        indicatorCodes.add("E12");
+        indicatorCodes.add("F12");
+        indicatorCodes.add("G12");
+        indicatorCodes.add("H12");
+        indicatorCodes.add("I12");
+        indicatorCodes.add("J12");
+        indicatorCodes.add("K12");
+        indicatorCodes.add("L12");
+        indicatorCodes.add("M12");
+        indicatorCodes.add("N12");
+        indicatorCodes.add("O12");
+        indicatorCodes.add("P12");
+        indicatorCodes.add("Q12a");
+        indicatorCodes.add("Q12b");
+    }
+
+    @Override
+    public JSONObject getIndicatorData() throws JSONException {
+        JSONObject indicatorDataObject = new JSONObject();
+        for (String indicatorCode : indicatorCodes) {
+            indicatorDataObject.put(indicatorCode, PmtctReportDao.getPmtctReportPerIndicatorCode(indicatorCode, reportDate));
+        }
+        df.setMaximumFractionDigits(2);
+
+        indicatorDataObject.put("D12", getIndicatorD12());
+        indicatorDataObject.put("K12", df.format(getIndicatorK12()) + "%");
+
+        return indicatorDataObject;
+    }
+
+    private int getIndicatorD12() {
+        //D12 = A12 + B12 - C12
+        return PmtctReportDao.getPmtctReportPerIndicatorCode("A12", reportDate)
+                + PmtctReportDao.getPmtctReportPerIndicatorCode("B12", reportDate)
+                - PmtctReportDao.getPmtctReportPerIndicatorCode("C12", reportDate);
+    }
+
+    private float getIndicatorK12() {
+        // K12 = E12 /(D12-J12) * 100
+        if (PmtctReportDao.getPmtctReportPerIndicatorCode("D12", reportDate) - PmtctReportDao.getPmtctReportPerIndicatorCode("J12", reportDate) > 0) {
+            return ((PmtctReportDao.getPmtctReportPerIndicatorCode("E12", reportDate) * 1f) / (getIndicatorD12() - PmtctReportDao.getPmtctReportPerIndicatorCode("J12", reportDate))) * 100;
+        }
+        return 0;
+    }
+
+
+}
---
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/domain/pmtctReports/Pmtct3MonthsReportObject.java
@@ -0,0 +1,94 @@
+package org.smartregister.chw.hf.domain.pmtctReports;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.chw.hf.dao.PmtctReportDao;
+import org.smartregister.chw.hf.domain.ReportObject;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Pmtct3MonthsReportObject extends ReportObject {
+    DecimalFormat df = new DecimalFormat();
+    ;
+    private List<String> indicatorCodes = new ArrayList<>();
+    private Date reportDate;
+
+    public Pmtct3MonthsReportObject(Date reportDate) {
+        super(reportDate);
+        this.reportDate = reportDate;
+        setIndicatorCodes(indicatorCodes);
+    }
+
+    public void setIndicatorCodes(List<String> indicatorCodes) {
+        indicatorCodes.add("B3a");
+        indicatorCodes.add("B3b");
+        indicatorCodes.add("B3c");
+        indicatorCodes.add("B3d");
+        indicatorCodes.add("C3a");
+        indicatorCodes.add("C3b");
+        indicatorCodes.add("D3");
+        indicatorCodes.add("E3");
+        indicatorCodes.add("G3");
+        indicatorCodes.add("H3");
+        indicatorCodes.add("I3");
+        indicatorCodes.add("J3");
+    }
+
+    @Override
+    public JSONObject getIndicatorData() throws JSONException {
+        JSONObject indicatorDataObject = new JSONObject();
+        for (String indicatorCode : indicatorCodes) {
+            indicatorDataObject.put(indicatorCode, PmtctReportDao.getPmtctReportPerIndicatorCode(indicatorCode, reportDate));
+        }
+        df.setMaximumFractionDigits(2);
+        //F3 = A3 + D3 - E3
+        indicatorDataObject.put("F3", getIndicatorA3() + getIndicatorD3() - getIndicatorE3());
+        indicatorDataObject.put("K3", df.format(getIndicatorK3()) + "%");
+        return indicatorDataObject;
+    }
+
+    private int getIndicatorA3() {
+        // A3 = B3a + B3b + B3c + B3d
+        return PmtctReportDao.getPmtctReportPerIndicatorCode("B3a", reportDate)
+                + PmtctReportDao.getPmtctReportPerIndicatorCode("B3b", reportDate)
+                + PmtctReportDao.getPmtctReportPerIndicatorCode("B3c", reportDate)
+                + PmtctReportDao.getPmtctReportPerIndicatorCode("B3d", reportDate);
+    }
+
+    private int getIndicatorD3() {
+        return PmtctReportDao.getPmtctReportPerIndicatorCode("D3", reportDate);
+    }
+
+    private int getIndicatorE3() {
+        return PmtctReportDao.getPmtctReportPerIndicatorCode("E3", reportDate);
+    }
+
+    private float getIndicatorK3() {
+        //if F3 - J3 < 0 return 0
+        //else K3 = G3 /(F3 - J3) * 100 to 2 decimal places
+        int denominator = getIndicatorF3() - getIndicatorJ3();
+        float numerator = getIndicatorG3() * 1f;
+        if (denominator > 0) {
+            return ((numerator) / (denominator)) * 100;
+        }
+        return 0;
+
+    }
+
+    private int getIndicatorF3() {
+        return PmtctReportDao.getPmtctReportPerIndicatorCode("F3", reportDate);
+    }
+
+    private int getIndicatorJ3() {
+        return PmtctReportDao.getPmtctReportPerIndicatorCode("J3", reportDate);
+    }
+
+    private int getIndicatorG3() {
+        return PmtctReportDao.getPmtctReportPerIndicatorCode("G3", reportDate);
+    }
+
+
+}
