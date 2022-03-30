0904117b502b9476850f951b1d6eff82b168ed26
Billy <billypatrick767@gmail.com>
2022-03-30 10:23:28 +0300

    fix: code styles and formatting

--- opensrp-chw-hf/src/main/assets/reports/report-script.js
+++ opensrp-chw-hf/src/main/assets/reports/report-script.js
@@ -1,13 +1,14 @@
 function loadData(reportKey) {
   const data = JSON.parse(Android.getData(reportKey));
   const keys = Object.keys(data.nameValuePairs);
+  const reportPeriod = document.getElementById("report_period");
+  const reportingFacility = document.getElementById("reporting_facility");
   keys.forEach((key) => {
     const element = document.getElementById(key);
-    if (element !== null && element !== undefined) {
-      document.getElementById(key).innerHTML = data.nameValuePairs[key];
+    if (element !== null && typeof element !== "undefined") {
+      element.innerHTML = data.nameValuePairs[key];
     }
   });
-  document.getElementById("report_period").innerHTML = Android.getDataPeriod();
-  document.getElementById("reporting_facility").innerHTML =
-    Android.getReportingFacility();
+  reportingFacility.innerHTML = Android.getDataPeriod();
+  reportPeriod.innerHTML = Android.getReportingFacility();
 }
