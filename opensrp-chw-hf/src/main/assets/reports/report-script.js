8c2e5298394ed03d93ed2371afc24a2bd277cea9
Billy <billypatrick767@gmail.com>
2022-03-30 09:52:03 +0300

    refactor: css and js code cleaning

--- opensrp-chw-hf/src/main/assets/reports/report-script.js
+++ opensrp-chw-hf/src/main/assets/reports/report-script.js
@@ -1,13 +1,13 @@
 function loadData(reportKey) {
-        const data = JSON.parse(Android.getData(reportKey));
-        const keys = Object.keys(data.nameValuePairs)
-        keys.forEach(key => {
-            const element = document.getElementById(key)
-            if (element !== null && element !== undefined) {
-                document.getElementById(key).innerHTML = data.nameValuePairs[key]
-            }
-        })
-        document.getElementById("report_period").innerHTML = Android.getDataPeriod();
-        document.getElementById("reporting_facility").innerHTML = Android.getReportingFacility();
+  const data = JSON.parse(Android.getData(reportKey));
+  const keys = Object.keys(data.nameValuePairs);
+  keys.forEach((key) => {
+    const element = document.getElementById(key);
+    if (element !== null && element !== undefined) {
+      document.getElementById(key).innerHTML = data.nameValuePairs[key];
+    }
+  });
+  document.getElementById("report_period").innerHTML = Android.getDataPeriod();
+  document.getElementById("reporting_facility").innerHTML =
+    Android.getReportingFacility();
 }
-
