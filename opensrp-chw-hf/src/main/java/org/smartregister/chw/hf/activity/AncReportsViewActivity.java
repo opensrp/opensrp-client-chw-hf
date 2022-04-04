a44f954d0c10043a5e19acc75c0a2bfaca094e1a
Billy <billypatrick767@gmail.com>
2022-04-04 17:04:23 +0300

    fix: made reports view activities to extend general view

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/AncReportsViewActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/AncReportsViewActivity.java
@@ -1,174 +1,18 @@
 package org.smartregister.chw.hf.activity;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Build;
-import android.view.Menu;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-
-import com.google.android.material.appbar.AppBarLayout;
 
 import org.smartregister.chw.hf.R;
-import org.smartregister.chw.hf.domain.anc_reports.AncMonthlyReportObject;
-import org.smartregister.view.activity.SecuredActivity;
-import org.smartregister.view.customcontrols.CustomFontTextView;
+import org.smartregister.chw.hf.utils.Constants;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
-import androidx.webkit.WebViewAssetLoader;
-import androidx.webkit.WebViewClientCompat;
-import timber.log.Timber;
-
-import static org.smartregister.util.Utils.getAllSharedPreferences;
-
-public class AncReportsViewActivity extends SecuredActivity {
-
-    private static final String ARG_REPORT_NAME = "ARG_REPORT_NAME";
-    private static final String ARG_REPORT_DATE = "ARG_REPORT_DATE";
-    private static Date reportDate;
-    private static String reportPeriod;
-    protected CustomFontTextView toolBarTextView;
-    protected AppBarLayout appBarLayout;
-
-    public static void startMe(Activity activity, String reportName, String reportDate) {
-        Intent intent = new Intent(activity, AncReportsViewActivity.class);
-        intent.putExtra(ARG_REPORT_NAME, reportName);
+public class AncReportsViewActivity extends HfReportsViewActivity {
+    public static void startMe(Activity activity, String reportPath, String reportDate) {
+        Intent intent = new Intent(activity, PncReportsViewActivity.class);
+        intent.putExtra(ARG_REPORT_PATH, reportPath);
         intent.putExtra(ARG_REPORT_DATE, reportDate);
+        intent.putExtra(ARG_REPORT_TITLE, R.string.anc_reports_title);
+        intent.putExtra(ARG_REPORT_TYPE, Constants.ReportConstants.ReportTypes.ANC_REPORT);
         activity.startActivity(intent);
-    }
-
-    @Override
-    protected void onCreation() {
-        setContentView(R.layout.activity_anc_reports_view);
-        String reportName = getIntent().getStringExtra(ARG_REPORT_NAME);
-        try {
-            reportDate = new SimpleDateFormat("MM-yyyy", Locale.getDefault()).parse(getIntent().getStringExtra(ARG_REPORT_DATE));
-        } catch (ParseException e) {
-            Timber.e(e);
-        }
-        setUpToolbar();
-        loadReportView(reportName);
-    }
-
-    public void setUpToolbar() {
-        Toolbar toolbar = findViewById(org.smartregister.chw.core.R.id.back_to_nav_toolbar);
-        toolBarTextView = toolbar.findViewById(org.smartregister.chw.core.R.id.toolbar_title);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            final Drawable upArrow = getResources().getDrawable(org.smartregister.chw.core.R.drawable.ic_arrow_back_white_24dp);
-            actionBar.setHomeAsUpIndicator(upArrow);
-            actionBar.setElevation(0);
-        }
-        toolbar.setNavigationOnClickListener(v -> finish());
-
-        toolBarTextView.setText(R.string.anc_reports_title);
-
-        toolBarTextView.setOnClickListener(v -> finish());
-        appBarLayout = findViewById(org.smartregister.chw.core.R.id.app_bar);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            appBarLayout.setOutlineProvider(null);
-        }
-    }
-
-    @Override
-    protected void onResumption() {
-        //overridden
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return false;
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private void loadReportView(String reportName) {
-        WebView mWebView = findViewById(R.id.webview);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
-                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
-                .build();
-        mWebView.setWebViewClient(new LocalContentWebViewClient(assetLoader));
-        mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
-        mWebView.loadUrl("https://appassets.androidplatform.net/assets/reports/" + reportName + ".html");
-    }
-
-    private String computeAncReport(Date now) {
-        String report = "";
-        AncMonthlyReportObject ancMonthlyReportObject = new AncMonthlyReportObject(now);
-        try {
-            report = ancMonthlyReportObject.getIndicatorDataAsGson(ancMonthlyReportObject.getIndicatorData());
-        } catch (Exception e) {
-            Timber.e(e);
-        }
-        return report;
-    }
-
-    private static class LocalContentWebViewClient extends WebViewClientCompat {
-
-        private final WebViewAssetLoader mAssetLoader;
-
-        LocalContentWebViewClient(WebViewAssetLoader assetLoader) {
-            mAssetLoader = assetLoader;
-        }
-
-        @Override
-        @RequiresApi(21)
-        public WebResourceResponse shouldInterceptRequest(WebView view,
-                                                          WebResourceRequest request) {
-            return mAssetLoader.shouldInterceptRequest(request.getUrl());
-        }
-
-        @Override
-        @SuppressWarnings("deprecation") // to support API < 21
-        public WebResourceResponse shouldInterceptRequest(WebView view,
-                                                          String url) {
-            return mAssetLoader.shouldInterceptRequest(Uri.parse(url));
-        }
-
-    }
-
-    public class WebAppInterface {
-        Context mContext;
-
-        WebAppInterface(Context c) {
-            mContext = c;
-        }
-
-        @JavascriptInterface
-        public String getData(String key) {
-
-            if (key.equals("monthly")) {
-                return computeAncReport(reportDate);
-            }
-            return "";
-        }
-
-        @JavascriptInterface
-        public String getDataPeriod() {
-            return reportPeriod;
-        }
-
-        @JavascriptInterface
-        public String getReportingFacility() {
-            return getAllSharedPreferences().fetchCurrentLocality();
-        }
     }
 }
--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/PmtctReportsViewActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/PmtctReportsViewActivity.java
@@ -2,101 +2,16 @@ package org.smartregister.chw.hf.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.Toast;
 
-import com.google.android.material.appbar.AppBarLayout;
+import org.smartregister.chw.hf.utils.Constants;
 
-import org.apache.commons.lang3.StringUtils;
-import org.smartregister.chw.hf.R;
-import org.smartregister.chw.hf.utils.ReportUtils;
-import org.smartregister.view.customcontrols.CustomFontTextView;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-public class PmtctReportsViewActivity extends AppCompatActivity {
-    private static final String ARG_REPORT_PATH = "ARG_REPORT_PATH";
-    private static final String ARG_REPORT_TITLE = "ARG_REPORT_TITLE";
-    private static final String ARG_REPORT_DATE = "ARG_REPORT_DATE";
-    public static WebView printWebView;
-    protected CustomFontTextView toolBarTextView;
-    protected AppBarLayout appBarLayout;
-
+public class PmtctReportsViewActivity extends HfReportsViewActivity {
     public static void startMe(Activity activity, String reportPath, int reportTitle, String reportDate) {
         Intent intent = new Intent(activity, PmtctReportsViewActivity.class);
         intent.putExtra(ARG_REPORT_PATH, reportPath);
         intent.putExtra(ARG_REPORT_TITLE, reportTitle);
         intent.putExtra(ARG_REPORT_DATE, reportDate);
+        intent.putExtra(ARG_REPORT_TYPE, Constants.ReportConstants.ReportTypes.PMTCT_REPORT);
         activity.startActivity(intent);
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pmtct_reports_view);
-        String reportPath = getIntent().getStringExtra(ARG_REPORT_PATH);
-        int reportTitle = getIntent().getIntExtra(ARG_REPORT_TITLE, 0);
-        setUpToolbar(reportTitle);
-        WebView webView = findViewById(R.id.webview);
-        ReportUtils.setReportPeriod(getIntent().getStringExtra(ARG_REPORT_DATE));
-        ReportUtils.loadReportView(reportPath, webView, this);
-    }
-
-    public void setUpToolbar(int reportTitle) {
-        Toolbar toolbar = findViewById(org.smartregister.chw.core.R.id.back_to_nav_toolbar);
-        toolBarTextView = toolbar.findViewById(org.smartregister.chw.core.R.id.toolbar_title);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            final Drawable upArrow = getResources().getDrawable(org.smartregister.chw.core.R.drawable.ic_arrow_back_white_24dp);
-            actionBar.setHomeAsUpIndicator(upArrow);
-            actionBar.setElevation(0);
-        }
-        toolbar.setNavigationOnClickListener(v -> finish());
-        if (StringUtils.isNotBlank(getString(reportTitle))) {
-            toolBarTextView.setText(getString(reportTitle));
-        } else {
-            toolBarTextView.setText(R.string.pmtct_reports_title);
-        }
-        toolBarTextView.setOnClickListener(v -> finish());
-        appBarLayout = findViewById(org.smartregister.chw.core.R.id.app_bar);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            appBarLayout.setOutlineProvider(null);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.reports_view_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
-        if (itemId == R.id.action_print) {
-            if (printWebView != null) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    ReportUtils.printTheWebPage(printWebView, PmtctReportsViewActivity.this);
-                } else {
-                    Toast.makeText(this, "Not available for device below Android LOLLIPOP", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(this, "WebPage not fully loaded", Toast.LENGTH_SHORT).show();
-            }
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
 }
