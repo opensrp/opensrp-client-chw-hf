30db88e473d7ebbc2889c9f8f0c6100fe9846660
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-04-22 06:08:05 +0300

    refactor/ANC birth and emergency plan option for selecting birth delivery facility

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/AncFirstFacilityVisitInteractorFlv.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/AncFirstFacilityVisitInteractorFlv.java
@@ -74,7 +74,7 @@ public class AncFirstFacilityVisitInteractorFlv implements AncFirstFacilityVisit
                     }
                 }
                 JSONArray tree = referralHealthFacilities.getJSONArray("tree");
-                String parentTagName = "Zone";
+                String parentTagName = "Region";
                 for (Location location : locations) {
                     Set<LocationTag> locationTags = location.getLocationTags();
                     if (locationTags.iterator().next().getName().equalsIgnoreCase(parentTagName)) {
