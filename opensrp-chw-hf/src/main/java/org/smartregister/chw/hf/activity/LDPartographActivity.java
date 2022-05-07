e1f0e498046e73dfab545f11d57824f52e2f44a3
issyzac <issyzac.iz@gmail.com>
2022-05-07 20:59:59 +0300

    :construction: add implementation for partograph activity

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/LDPartographActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/LDPartographActivity.java
@@ -1,11 +1,30 @@
 package org.smartregister.chw.hf.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.domain.Form;
+
+import org.json.JSONObject;
+import org.smartregister.chw.core.task.RunnableTask;
+import org.smartregister.chw.hf.R;
+import org.smartregister.chw.hf.interactor.LDPartographInteractor;
 import org.smartregister.chw.hf.interactor.LDRegistrationInteractor;
+import org.smartregister.chw.hf.schedulers.HfScheduleTaskExecutor;
+import org.smartregister.chw.hf.utils.Constants;
 import org.smartregister.chw.ld.activity.BaseLDVisitActivity;
+import org.smartregister.chw.ld.domain.MemberObject;
 import org.smartregister.chw.ld.presenter.BaseLDVisitPresenter;
+import org.smartregister.family.util.JsonFormUtils;
+import org.smartregister.family.util.Utils;
+import org.smartregister.util.LangUtils;
+
+import java.text.MessageFormat;
+import java.util.Date;
+
+import timber.log.Timber;
 
 /**
  * @author issyzac 5/7/22
@@ -13,7 +32,7 @@ import org.smartregister.chw.ld.presenter.BaseLDVisitPresenter;
 public class LDPartographActivity extends BaseLDVisitActivity {
 
     public static void startMe(Activity activity, String baseEntityID, Boolean isEditMode, String fullName, String age) {
-        Intent intent = new Intent(activity, LDRegistrationFormActivity.class);
+        Intent intent = new Intent(activity, LDPartographActivity.class);
         intent.putExtra(org.smartregister.chw.ld.util.Constants.ACTIVITY_PAYLOAD.BASE_ENTITY_ID, baseEntityID);
         intent.putExtra(org.smartregister.chw.ld.util.Constants.ACTIVITY_PAYLOAD.EDIT_MODE, isEditMode);
         intent.putExtra("FULL_NAME", fullName);
@@ -23,7 +42,51 @@ public class LDPartographActivity extends BaseLDVisitActivity {
 
     @Override
     protected void registerPresenter() {
-        presenter = new BaseLDVisitPresenter(memberObject, this, new LDRegistrationInteractor(baseEntityID));
+        presenter = new BaseLDVisitPresenter(memberObject, this, new LDPartographInteractor(baseEntityID));
     }
+
+    @Override
+    public void submittedAndClose() {
+        Runnable runnable = () -> HfScheduleTaskExecutor.getInstance().execute(memberObject.getBaseEntityId(), Constants.Events.LD_PARTOGRAPHY, new Date());
+        Utils.startAsyncTask(new RunnableTask(runnable), null);
+        super.submittedAndClose();
+    }
+
+
+    @Override
+    public void startFormActivity(JSONObject jsonForm) {
+
+        Form form = new Form();
+        form.setActionBarBackground(org.smartregister.chw.core.R.color.family_actionbar);
+        form.setWizard(false);
+
+        Intent intent = new Intent(this, Utils.metadata().familyMemberFormActivity);
+        intent.putExtra(org.smartregister.family.util.Constants.JSON_FORM_EXTRA.JSON, jsonForm.toString());
+        intent.putExtra(org.smartregister.family.util.Constants.WizardFormActivity.EnableOnCloseDialog, false);
+        intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
+        startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        // get language from prefs
+        String lang = LangUtils.getLanguage(base.getApplicationContext());
+        super.attachBaseContext(LangUtils.setAppLocale(base, lang));
+    }
+
+    @Override
+    public void redrawHeader(MemberObject memberObject) {
+        tvTitle.setText(MessageFormat.format("{0}, {1} \u00B7 {2}", getIntent().getStringExtra("FULL_NAME"), getIntent().getStringExtra("AGE"), getString(R.string.ld_partograph)));
+    }
+
+    @Override
+    public void onDestroy() {
+        try {
+            super.onDestroy();
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
 
 }
