742d83749a12af15b5d6d98c739b16f47f39c072
Billy <billypatrick767@gmail.com>
2022-05-11 20:44:05 +0300

    fix: method for getting visits to get more than one visit

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/AncMedicalHistoryInteractor.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/AncMedicalHistoryInteractor.java
@@ -4,11 +4,17 @@ import android.content.Context;
 
 import org.smartregister.chw.anc.contract.BaseAncMedicalHistoryContract;
 import org.smartregister.chw.anc.domain.Visit;
+import org.smartregister.chw.anc.domain.VisitDetail;
 import org.smartregister.chw.anc.util.VisitUtils;
 import org.smartregister.chw.core.CoreBaseAncMedicalHistoryInteractor;
+import org.smartregister.chw.hf.utils.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.smartregister.chw.anc.util.VisitUtils.getVisitDetailsOnly;
+import static org.smartregister.chw.anc.util.VisitUtils.getVisitGroups;
+import static org.smartregister.chw.anc.util.VisitUtils.getVisitsOnly;
 
 public class AncMedicalHistoryInteractor extends CoreBaseAncMedicalHistoryInteractor {
     @Override
@@ -16,9 +22,9 @@ public class AncMedicalHistoryInteractor extends CoreBaseAncMedicalHistoryIntera
         final Runnable runnable = () -> {
 
             String[] eventTypes = new String[2];
-            eventTypes[0] = "ANC First Facility Visit";
-            eventTypes[1] = "ANC Recurring Facility Visit";
-            List<Visit> visits = VisitUtils.getVisits(memberID, eventTypes);
+            eventTypes[0] = Constants.Events.ANC_FIRST_FACILITY_VISIT;
+            eventTypes[1] = Constants.Events.ANC_RECURRING_FACILITY_VISIT;
+            List<Visit> visits = getVisits(memberID, eventTypes);
             final List<Visit> all_visits = new ArrayList<>(visits);
 
             for (Visit visit : visits) {
@@ -29,5 +35,28 @@ public class AncMedicalHistoryInteractor extends CoreBaseAncMedicalHistoryIntera
         };
 
         appExecutors.diskIO().execute(runnable);
+    }
+
+    public static List<Visit> getVisits(String memberID, String... eventTypes) {
+
+        List<Visit> visits = new ArrayList<>();
+              if(eventTypes != null && eventTypes.length > 0) {
+                    for (String eventType : eventTypes) {
+                        List<Visit>  visit= getVisitsOnly(memberID, eventType);
+                        visits.addAll(visit);
+                    }
+              } else{
+                getVisitsOnly(memberID, Constants.Events.ANC_FIRST_FACILITY_VISIT);
+              }
+
+        int x = 0;
+        while (visits.size() > x) {
+            Visit visit = visits.get(x);
+            List<VisitDetail> detailList = getVisitDetailsOnly(visit.getVisitId());
+            visits.get(x).setVisitDetails(getVisitGroups(detailList));
+            x++;
+        }
+
+        return visits;
     }
 }
