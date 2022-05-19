0c16b5a905f6bb0c26726ba12c6baecb4dd9a014
issyzac <issyzac.iz@gmail.com>
2022-05-19 16:43:23 +0300

    :hammer: fix capture drugs and iv fluid

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/actionhelper/LDPartographTreatmentDuringLabourActionHelper.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/actionhelper/LDPartographTreatmentDuringLabourActionHelper.java
@@ -46,6 +46,8 @@ public class LDPartographTreatmentDuringLabourActionHelper implements BaseLDVisi
         try {
             JSONObject jsonObject = new JSONObject(jsonPayload);
             drugsAdministered = CoreJsonFormUtils.getValue(jsonObject, "drugs_administered");
+            drugs = CoreJsonFormUtils.getValue(jsonObject, "drugs");
+            ivFluid = CoreJsonFormUtils.getValue(jsonObject, "iv_fluid");
         }catch (Exception e){
             Timber.e(e);
         }
