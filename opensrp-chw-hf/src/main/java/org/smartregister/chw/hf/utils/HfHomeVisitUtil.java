82b62e6ca9c06ab8bf2e868aa3e863341043494b
Billy <billypatrick767@gmail.com>
2022-05-25 21:14:27 +0300

    update: HomeVisitUtil with PmtctRule

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/HfHomeVisitUtil.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/utils/HfHomeVisitUtil.java
@@ -1,9 +1,11 @@
 package org.smartregister.chw.hf.utils;
 
 import org.smartregister.chw.core.application.CoreChwApplication;
+import org.smartregister.chw.core.rule.PmtctFollowUpRule;
 import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.core.utils.HomeVisitUtil;
 import org.smartregister.chw.hf.rule.HfHeiFollowupRule;
+import org.smartregister.chw.hf.rule.HfPmtctFollowupRule;
 
 import java.util.Date;
 
@@ -12,5 +14,11 @@ public class HfHomeVisitUtil extends HomeVisitUtil {
         HfHeiFollowupRule heiFollowupRule = new HfHeiFollowupRule(heiStartDate, followupDate, baseEntityId);
         CoreChwApplication.getInstance().getRulesEngineHelper().getHeiRule(heiFollowupRule, CoreConstants.RULE_FILE.HEI_FOLLOWUP_VISIT);
         return heiFollowupRule;
+    }
+
+    public static PmtctFollowUpRule getPmtctVisitStatus(Date pmtctRegisterDate, Date followUpDate, String baseEntityId) {
+        PmtctFollowUpRule pmtctFollowUpRule = new HfPmtctFollowupRule(pmtctRegisterDate, followUpDate, baseEntityId);
+        CoreChwApplication.getInstance().getRulesEngineHelper().getPmtctRule(pmtctFollowUpRule, CoreConstants.RULE_FILE.PMTCT_FOLLOW_UP_VISIT);
+        return pmtctFollowUpRule;
     }
 }
