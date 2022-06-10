e7eeb3fbd23f484157b30c439363f57d3a24a3fd
Billy <billypatrick767@gmail.com>
2022-06-10 11:21:16 +0300

    rollback: referral model to its initial state

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/model/ReferralModel.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/model/ReferralModel.java
@@ -18,8 +18,6 @@ public class ReferralModel extends BaseReferralModel {
         queryBuilder.selectInitiateMainTable(tableName, mainColumns(tableName, entityTable), CoreConstants.DB_CONSTANTS.ID);
         queryBuilder.customJoin(String.format("INNER JOIN %s  ON  %s.%s = %s.%s  AND task.business_status = 'Referred'  COLLATE NOCASE ",
                 entityTable, entityTable, DBConstants.KEY.BASE_ENTITY_ID, tableName, CoreConstants.DB_CONSTANTS.FOR));
-        queryBuilder.customJoin(String.format("INNER JOIN %s  ON  %s.%s = %s.%s COLLATE NOCASE ", CoreConstants.TABLE_NAME.REFERRAL,
-                CoreConstants.TABLE_NAME.REFERRAL, DBConstants.KEY.BASE_ENTITY_ID, tableName, ChwDBConstants.TaskTable.REASON_REFERENCE));
         queryBuilder.customJoin("LEFT JOIN ec_family  ON  ec_family_member.relational_id = ec_family.id COLLATE NOCASE");
         return queryBuilder.mainCondition(mainCondition);
     }
@@ -41,11 +39,6 @@ public class ReferralModel extends BaseReferralModel {
         columns.add(table + "." + DBConstants.KEY.DOB);
         columns.add(table + "." + DBConstants.KEY.GENDER);
         columns.add(CoreConstants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FAMILY_HEAD);
-        columns.add(CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.UNIQUE_ID);
-        columns.add(CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID);
-        columns.add(CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.PHONE_NUMBER);
-        columns.add(CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.OTHER_PHONE_NUMBER);
-        columns.add(CoreConstants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.VILLAGE_TOWN);
 
     }
 
@@ -54,12 +47,6 @@ public class ReferralModel extends BaseReferralModel {
         columns.add(CoreConstants.TABLE_NAME.TASK + "." + CoreConstants.DB_CONSTANTS.OWNER);
         columns.add(CoreConstants.TABLE_NAME.TASK + "." + CoreConstants.DB_CONSTANTS.REQUESTER);
         columns.add(CoreConstants.TABLE_NAME.TASK + "." + CoreConstants.DB_CONSTANTS.START);
-        columns.add(CoreConstants.TABLE_NAME.TASK + "." + CoreConstants.DB_CONSTANTS.BUSINESS_STATUS + " AS " + org.smartregister.chw.referral.util.DBConstants.Key.REFERRAL_STATUS);
-        columns.add(CoreConstants.TABLE_NAME.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.Key.REFERRAL_HF);
-        columns.add(CoreConstants.TABLE_NAME.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.Key.PROBLEM);
-        columns.add(CoreConstants.TABLE_NAME.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.Key.REFERRAL_SERVICE);
-        columns.add(CoreConstants.TABLE_NAME.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.Key.REFERRAL_DATE);
-        columns.add(CoreConstants.TABLE_NAME.TASK + "." + CoreConstants.DB_CONSTANTS.FOCUS + " AS " + org.smartregister.chw.referral.util.DBConstants.Key.REFERRAL_TYPE);
 
     }
 }
