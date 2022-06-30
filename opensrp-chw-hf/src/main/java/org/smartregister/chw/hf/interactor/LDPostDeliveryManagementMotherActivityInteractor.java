2fa5e154941759a9ac2ecca406ab8d4e22eb6fe7
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-06-30 12:22:40 +0300

    :hammer: refactored LD issues

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/LDPostDeliveryManagementMotherActivityInteractor.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/LDPostDeliveryManagementMotherActivityInteractor.java
@@ -179,8 +179,6 @@ public class LDPostDeliveryManagementMotherActivityInteractor extends BaseLDVisi
         private String delivery_place;
         private String designation_of_delivery_personnel;
         private String name_of_delivery_person;
-        private String supervised_by_occupation;
-        private String name_of_supervising_person;
         private String delivery_date;
         private String completionStatus;
         private int numberOfChildrenBorn = 0;
@@ -216,8 +214,6 @@ public class LDPostDeliveryManagementMotherActivityInteractor extends BaseLDVisi
             time_of_death = JsonFormUtils.getFieldValue(jsonPayload, "time_of_death");
             delivery_place = JsonFormUtils.getFieldValue(jsonPayload, "delivery_place");
             designation_of_delivery_personnel = JsonFormUtils.getFieldValue(jsonPayload, "designation_of_delivery_personnel");
-            supervised_by_occupation = JsonFormUtils.getFieldValue(jsonPayload, "supervised_by_occupation");
-            name_of_supervising_person = JsonFormUtils.getFieldValue(jsonPayload, "name_of_supervising_person");
             name_of_delivery_person = JsonFormUtils.getFieldValue(jsonPayload, "name_of_delivery_person");
             delivery_date = JsonFormUtils.getFieldValue(jsonPayload, "delivery_date");
             delivery_time = JsonFormUtils.getFieldValue(jsonPayload, "delivery_time");
@@ -360,13 +356,13 @@ public class LDPostDeliveryManagementMotherActivityInteractor extends BaseLDVisi
                     if ((StringUtils.isNotBlank(delivery_place) && !delivery_place.equalsIgnoreCase("Place of delivery")) &&
                             StringUtils.isNotBlank(delivery_date)) {
                         completed = !delivery_place.equalsIgnoreCase("At a health facility") || (StringUtils.isNotBlank(designation_of_delivery_personnel) &&
-                                StringUtils.isNotBlank(name_of_delivery_person) && StringUtils.isNotBlank(supervised_by_occupation) && StringUtils.isNotBlank(name_of_supervising_person));
+                                StringUtils.isNotBlank(name_of_delivery_person));
                     }
                 } else {
                     if ((StringUtils.isNotBlank(delivery_place) && !delivery_place.equalsIgnoreCase("Place of delivery")) &&
                             StringUtils.isNotBlank(cause_of_death) && StringUtils.isNotBlank(time_of_death) && StringUtils.isNotBlank(delivery_date)) {
                         completed = !delivery_place.equalsIgnoreCase("At a health facility") || (StringUtils.isNotBlank(designation_of_delivery_personnel) &&
-                                StringUtils.isNotBlank(name_of_delivery_person) && StringUtils.isNotBlank(supervised_by_occupation) && StringUtils.isNotBlank(name_of_supervising_person));
+                                StringUtils.isNotBlank(name_of_delivery_person));
                     }
                 }
             }
@@ -381,14 +377,14 @@ public class LDPostDeliveryManagementMotherActivityInteractor extends BaseLDVisi
                     if (delivery_place.equalsIgnoreCase("Place of delivery") || StringUtils.isBlank(delivery_date)) {
                         partialCompletion = true;
                     } else if (delivery_place.equalsIgnoreCase("At a health facility")) {
-                        partialCompletion = StringUtils.isBlank(designation_of_delivery_personnel) || StringUtils.isBlank(name_of_delivery_person) || StringUtils.isBlank(supervised_by_occupation) || StringUtils.isBlank(name_of_supervising_person);
+                        partialCompletion = StringUtils.isBlank(designation_of_delivery_personnel) || StringUtils.isBlank(name_of_delivery_person);
                     }
                 } else {
                     if (delivery_place.equalsIgnoreCase("Place of delivery") || StringUtils.isBlank(delivery_date) || StringUtils.isBlank(cause_of_death) ||
                             StringUtils.isBlank(time_of_death)) {
                         partialCompletion = true;
                     } else if (delivery_place.equalsIgnoreCase("At a health facility")) {
-                        partialCompletion = StringUtils.isBlank(designation_of_delivery_personnel) || StringUtils.isBlank(name_of_delivery_person) || StringUtils.isBlank(supervised_by_occupation) || StringUtils.isBlank(name_of_supervising_person);
+                        partialCompletion = StringUtils.isBlank(designation_of_delivery_personnel) || StringUtils.isBlank(name_of_delivery_person);
                     }
                 }
             }
