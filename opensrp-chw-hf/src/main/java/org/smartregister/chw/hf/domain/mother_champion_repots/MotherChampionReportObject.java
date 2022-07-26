5477f79e2a3055982245939ca52bb607b1906cd8
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-07-26 16:25:10 +0300

    :hammer: implemented report indicators for mother champion in-app reports

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/domain/mother_champion_repots/MotherChampionReportObject.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/domain/mother_champion_repots/MotherChampionReportObject.java
@@ -3,13 +3,15 @@ package org.smartregister.chw.hf.domain.mother_champion_repots;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.smartregister.chw.hf.dao.ReportDao;
 import org.smartregister.chw.hf.domain.ReportObject;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class MotherChampionReportObject extends ReportObject {
     final Date reportDate;
-    private final String[] motherChampionDetails = new String[]{"majina_mama_vinara", "namba_ya_simu", "jina_lililosajiliwa", "sahihi"};
     private final String[] indicatorCodes = new String[]{"b-1", "b-2", "b-3", "b-4", "b-5", "b-6", "b-7"};
 
     public MotherChampionReportObject(Date reportDate) {
@@ -20,49 +22,24 @@ public class MotherChampionReportObject extends ReportObject {
 
     @Override
     public JSONObject getIndicatorData() throws JSONException {
-        /*
-
-         * we'll pass a json object with the following structure:
-         * reportData = [
-            {
-              "id": "1",
-              "majina_mama_vinara": "name",
-              "namba_ya_simu": "number",
-              "jina_lililosajiliwa": "name",
-              "sahihi": "signed",
-             },
-          ]
-         * */
-        JSONObject jsonObject = new JSONObject();
         JSONArray dataArray = new JSONArray();
-        //Todo get an array of ids of mother champions from the db for that report time period
-        //something like : List<String> motherChampionIds = ReportDao.getAllMotherChampionIds(reportDate);
-        //Todo loop through the motherChampionIds and get the data for each motherChampion
-        //something like : for (String motherChampionId : motherChampionIds) {
-        //                      JSONObject motherChampionObject = new JSONObject();
-        //                      motherChampionObject.put("id", motherChampionId);
-        //                      for(String motherChampionDetail : motherChampionDetails) {
-        //                          motherChampionObject.put(motherChampionDetail, ReportDao.getMotherChampionDetail(motherChampionId, reportDate));
-        //                      }
-        //                      dataArray.put(motherChampionObject);
-        //                  }
 
-        //TODO add the dataArray to the jsonObject
+        List<Map<String, String>> motherChampionsList = ReportDao.getMotherChampions(reportDate);
 
-        //SAMPLE
-        JSONObject motherChampionObject = new JSONObject();
-        motherChampionObject.put("id", 1);
-        for (String motherChampionDetail : motherChampionDetails) {
-            motherChampionObject.put(motherChampionDetail, "test");
+        int i = 0;
+        for (Map<String, String> motherChampion : motherChampionsList) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", ++i);
+            jsonObject.put("majina_mama_vinara", motherChampion.get("chw_name"));
+            jsonObject.put("jina_lililosajiliwa", motherChampion.get("provider_id"));
+            dataArray.put(jsonObject);
         }
-        dataArray.put(motherChampionObject);
-        //END SAMPLE
 
+
+        JSONObject jsonObject = new JSONObject();
         jsonObject.put("reportData", dataArray);
         for (String indicatorCode : indicatorCodes) {
-            //jsonObject.put(indicatorCode, ReportDao.getReportPerIndicatorCode(indicatorCode));
-            //TODO: remove this for test purposes
-            jsonObject.put(indicatorCode, 10);
+            jsonObject.put(indicatorCode, ReportDao.getReportPerIndicatorCode(indicatorCode, reportDate));
         }
 
         return jsonObject;
