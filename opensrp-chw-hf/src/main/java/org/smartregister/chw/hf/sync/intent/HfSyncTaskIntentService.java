54e35993bb1b34e5cc053f75cd49af34f76e85c9
Ephraim Kigamba <ekigamba@ona.io>
2022-08-19 10:17:56 +0300

    Schedule SyncTaskWithClientEventsServiceJob 5 mins after Tasks sync
    - This prevents blocking the EventClient method during registration as discovered by Owais

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/sync/intent/HfSyncTaskIntentService.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/sync/intent/HfSyncTaskIntentService.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 
 import com.evernote.android.job.JobManager;
 import com.evernote.android.job.JobRequest;
-import com.evernote.android.job.util.support.PersistableBundleCompat;
 
-import org.smartregister.AllConstants;
 import org.smartregister.chw.core.job.CloseExpiredReferralsServiceJob;
 import org.smartregister.chw.core.job.SyncTaskWithClientEventsServiceJob;
 import org.smartregister.chw.hf.sync.helper.HfTaskServiceHelper;
@@ -21,28 +19,16 @@ public class HfSyncTaskIntentService extends SyncTaskIntentService {
     protected void onHandleIntent(Intent intent) {
         HfTaskServiceHelper taskServiceHelper = HfTaskServiceHelper.getInstance();
         taskServiceHelper.syncTasks();
-        scheduleJob(SyncTaskWithClientEventsServiceJob.TAG, 15L, 5L);
+        scheduleJobLater(SyncTaskWithClientEventsServiceJob.TAG, 5L);
         CloseExpiredReferralsServiceJob.scheduleJobImmediately(CloseExpiredReferralsServiceJob.TAG);
     }
 
-    public void scheduleJob(String jobTag, Long start, Long flex) {
-
+    public void scheduleJobLater(String jobTag, Long startMins) {
         if (JobManager.instance().getAllJobRequestsForTag(jobTag).isEmpty()) {
-
-            PersistableBundleCompat extras = new PersistableBundleCompat();
-            extras.putBoolean(AllConstants.INTENT_KEY.TO_RESCHEDULE, false);
-
-            JobRequest.Builder jobRequest = new JobRequest.Builder(jobTag)
-                    .setExtras(extras)
-                    .setPeriodic(TimeUnit.MINUTES.toMillis(start), TimeUnit.MINUTES.toMillis(flex));
-            try {
-
-                int jobId = jobRequest.build().schedule();
-                Timber.d("Scheduling job with name " + jobTag + " : JOB ID " + jobId + " periodically every " + start + " minutes and flex value of " + flex + " minutes");
-
-            } catch (Exception e) {
-                Timber.e(e);
-            }
+            new JobRequest.Builder(SyncTaskWithClientEventsServiceJob.TAG)
+                    .setExecutionWindow(TimeUnit.MINUTES.toMillis(startMins), TimeUnit.MINUTES.toMillis(startMins + 10))
+                    .build()
+                    .schedule();
         } else {
             Timber.d("Skipping schedule for job with name " + jobTag + " : Already Exists!");
         }
