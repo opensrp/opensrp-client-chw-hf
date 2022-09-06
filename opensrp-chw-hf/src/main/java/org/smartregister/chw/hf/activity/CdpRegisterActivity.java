af25e66b914a7e876c829d39a4b05b1fc6176dd6
Billy <billypatrick767@gmail.com>
2022-09-06 09:45:37 +0300

    fix: cleanup bottom navigation

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/CdpRegisterActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/CdpRegisterActivity.java
@@ -1,7 +1,12 @@
 package org.smartregister.chw.hf.activity;
 
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+
+import org.smartregister.chw.cdp.listener.BaseCdpBottomNavigationListener;
 import org.smartregister.chw.core.activity.CoreCdpRegisterActivity;
 import org.smartregister.chw.core.fragment.CoreOrdersRegisterFragment;
+import org.smartregister.helper.BottomNavigationHelper;
+import org.smartregister.listener.BottomNavigationListener;
 import org.smartregister.view.fragment.BaseRegisterFragment;
 
 public class CdpRegisterActivity extends CoreCdpRegisterActivity {
@@ -9,5 +14,24 @@ public class CdpRegisterActivity extends CoreCdpRegisterActivity {
     @Override
     protected BaseRegisterFragment getRegisterFragment() {
         return new CoreOrdersRegisterFragment();
+    }
+
+    @Override
+    protected void registerBottomNavigation() {
+        bottomNavigationHelper = new BottomNavigationHelper();
+        bottomNavigationView = findViewById(org.smartregister.R.id.bottom_navigation);
+
+        if (bottomNavigationView != null) {
+            bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.R.id.action_clients);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.cdp.R.id.action_register);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.R.id.action_search);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.R.id.action_library);
+            bottomNavigationView.inflateMenu(getMenuResource());
+            bottomNavigationHelper.disableShiftMode(bottomNavigationView);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.cdp.R.id.action_add_outlet);
+            BottomNavigationListener familyBottomNavigationListener = new BaseCdpBottomNavigationListener(this);
+            bottomNavigationView.setOnNavigationItemSelectedListener(familyBottomNavigationListener);
+        }
     }
 }
