9a8600c27aac626361d54599814975e52aa1b98f
Billy <billypatrick767@gmail.com>
2022-09-06 09:34:32 +0300

    fix: added the cdp orders table

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/CdpRegisterActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/CdpRegisterActivity.java
@@ -1,6 +1,13 @@
 package org.smartregister.chw.hf.activity;
 
 import org.smartregister.chw.core.activity.CoreCdpRegisterActivity;
+import org.smartregister.chw.core.fragment.CoreOrdersRegisterFragment;
+import org.smartregister.view.fragment.BaseRegisterFragment;
 
 public class CdpRegisterActivity extends CoreCdpRegisterActivity {
+
+    @Override
+    protected BaseRegisterFragment getRegisterFragment() {
+        return new CoreOrdersRegisterFragment();
+    }
 }
