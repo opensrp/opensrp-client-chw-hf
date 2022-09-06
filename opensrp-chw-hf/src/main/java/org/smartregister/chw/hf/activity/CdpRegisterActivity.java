d74c90deebfe1a0bbd79f3a354e7d1a63635b2ce
Billy <billypatrick767@gmail.com>
2022-09-06 09:20:35 +0300

    init: condom library in hf

---
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/CdpRegisterActivity.java
@@ -0,0 +1,6 @@
+package org.smartregister.chw.hf.activity;
+
+import org.smartregister.chw.core.activity.CoreCdpRegisterActivity;
+
+public class CdpRegisterActivity extends CoreCdpRegisterActivity {
+}
