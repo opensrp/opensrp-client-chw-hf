a54be5ab6b5713059bdfbe65ac016741f81d83bc
Billy <billypatrick767@gmail.com>
2022-09-06 10:15:15 +0300

    fix: update the displayed tab layout

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/RequestOrdersRegisterFragment.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/RequestOrdersRegisterFragment.java
@@ -1,5 +1,9 @@
 package org.smartregister.chw.hf.fragment;
 
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+
 import org.smartregister.chw.core.fragment.CoreOrdersRegisterFragment;
 import org.smartregister.chw.hf.R;
 
@@ -8,5 +12,12 @@ public class RequestOrdersRegisterFragment extends CoreOrdersRegisterFragment {
     @Override
     protected int getFragmentTitle() {
         return R.string.menu_cdp;
+    }
+
+    @Override
+    protected void setUpTabLayout(View view) {
+        TabLayout tabs = view.findViewById(R.id.requests_tab_layout);
+        view.findViewById(R.id.tab_layout).setVisibility(View.GONE);
+        tabs.setVisibility(View.VISIBLE);
     }
 }
