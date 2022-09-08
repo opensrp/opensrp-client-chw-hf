4fd0a5422e35088250f5a77a4e3fab7842658f23
Billy <billypatrick767@gmail.com>
2022-09-08 23:36:20 +0300

    fix: loading order form

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/OrdersRegisterFragment.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/OrdersRegisterFragment.java
@@ -1,9 +1,16 @@
 package org.smartregister.chw.hf.fragment;
 
+import android.content.Intent;
+
+import org.json.JSONObject;
+import org.smartregister.chw.cdp.util.Constants;
 import org.smartregister.chw.core.fragment.CoreOrdersRegisterFragment;
 import org.smartregister.chw.hf.activity.OrderDetailsActivity;
 import org.smartregister.chw.hf.presenter.OrdersRegisterFragmentPresenter;
+import org.smartregister.chw.hf.utils.JsonFormUtils;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
+
+import static org.smartregister.chw.core.utils.FormUtils.getStartFormActivity;
 
 public class OrdersRegisterFragment extends CoreOrdersRegisterFragment {
 
@@ -15,5 +22,15 @@ public class OrdersRegisterFragment extends CoreOrdersRegisterFragment {
     @Override
     public void showDetails(CommonPersonObjectClient cp) {
         OrderDetailsActivity.startMe(requireActivity(), cp);
+    }
+    @Override
+    public void startOrderForm() {
+        try {
+            JSONObject form = model().getOrderFormAsJson(Constants.FORMS.CDP_CONDOM_ORDER_FACILITY);
+            Intent startFormIntent = getStartFormActivity(form, null, requireActivity());
+            requireActivity().startActivityForResult(startFormIntent, JsonFormUtils.REQUEST_CODE_GET_JSON);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
