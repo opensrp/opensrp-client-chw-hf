f1ca529d8657e338fe7959470145132efab97639
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-09-27 11:53:54 +0300

    :construction: implement removing of PMTCT mothers after HEI child is marked as deceased

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/dao/HeiDao.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/dao/HeiDao.java
@@ -449,6 +449,57 @@ public class HeiDao extends AbstractDao {
         return res;
     }
 
+    public static List<MemberObject> getMembersByMotherBaseEntityId(String motherBaseEntityId) {
+        String sql = "select m.base_entity_id , m.unique_id , m.relational_id , m.dob , m.first_name , m.middle_name , m.last_name , m.gender , m.phone_number , m.other_phone_number , f.first_name family_name ,f.primary_caregiver , f.family_head , f.village_town ,fh.first_name family_head_first_name , fh.middle_name family_head_middle_name , fh.last_name family_head_last_name, fh.phone_number family_head_phone_number , ancr.is_closed anc_is_closed, pncr.is_closed pnc_is_closed, pcg.first_name pcg_first_name , pcg.last_name pcg_last_name , pcg.middle_name pcg_middle_name , pcg.phone_number  pcg_phone_number , mr.* from ec_family_member m inner join ec_family f on m.relational_id = f.base_entity_id inner join ec_hei mr on mr.base_entity_id = m.base_entity_id left join ec_family_member fh on fh.base_entity_id = f.family_head left join ec_family_member pcg on pcg.base_entity_id = f.primary_caregiver left join ec_anc_register ancr on ancr.base_entity_id = m.base_entity_id left join ec_pregnancy_outcome pncr on pncr.base_entity_id = m.base_entity_id where mr.is_closed = 0 AND mr.mother_entity_id = '"+motherBaseEntityId+"'";
+
+
+        DataMap<MemberObject> dataMap = cursor -> {
+            MemberObject memberObject = new MemberObject();
+
+            memberObject.setFirstName(getCursorValue(cursor, "first_name", ""));
+            memberObject.setMiddleName(getCursorValue(cursor, "middle_name", ""));
+            memberObject.setLastName(getCursorValue(cursor, "last_name", ""));
+            memberObject.setAddress(getCursorValue(cursor, "village_town"));
+            memberObject.setGender(getCursorValue(cursor, "gender"));
+            memberObject.setUniqueId(getCursorValue(cursor, "unique_id", ""));
+            memberObject.setAge(getCursorValue(cursor, "dob"));
+            memberObject.setDod(getCursorValue(cursor, "dod", null));
+            memberObject.setFamilyBaseEntityId(getCursorValue(cursor, "relational_id", ""));
+            memberObject.setRelationalId(getCursorValue(cursor, "relational_id", ""));
+            memberObject.setPrimaryCareGiver(getCursorValue(cursor, "primary_caregiver"));
+            memberObject.setFamilyName(getCursorValue(cursor, "family_name", ""));
+            memberObject.setPhoneNumber(getCursorValue(cursor, "phone_number", ""));
+            memberObject.setBaseEntityId(getCursorValue(cursor, "base_entity_id", ""));
+            memberObject.setFamilyHead(getCursorValue(cursor, "family_head", ""));
+            memberObject.setFamilyHeadPhoneNumber(getCursorValue(cursor, "pcg_phone_number", ""));
+            memberObject.setFamilyHeadPhoneNumber(getCursorValue(cursor, "family_head_phone_number", ""));
+            memberObject.setAncMember(getCursorValue(cursor, "anc_is_closed", ""));
+            memberObject.setPncMember(getCursorValue(cursor, "pnc_is_closed", ""));
+
+            String familyHeadName = getCursorValue(cursor, "family_head_first_name", "") + " "
+                    + getCursorValue(cursor, "family_head_middle_name", "");
+
+            familyHeadName =
+                    (familyHeadName.trim() + " " + getCursorValue(cursor, "family_head_last_name", "")).trim();
+            memberObject.setFamilyHeadName(familyHeadName);
+
+            String familyPcgName = getCursorValue(cursor, "pcg_first_name", "") + " "
+                    + getCursorValue(cursor, "pcg_middle_name", "");
+
+            familyPcgName =
+                    (familyPcgName.trim() + " " + getCursorValue(cursor, "pcg_last_name", "")).trim();
+            memberObject.setPrimaryCareGiverName(familyPcgName);
+
+            return memberObject;
+        };
+
+        List<MemberObject> res = readData(sql, dataMap);
+        if (res == null || res.size() == 0)
+            return null;
+
+        return res;
+    }
+
     public static void saveAntiBodyTestResults(String baseEntityID, String formSubmissionId, String hivTestResults, String hivTestResultsDate, String ctcNumber) {
         String sql = String.format("INSERT INTO ec_hei_hiv_results (id, entity_id, base_entity_id, hei_followup_form_submission_id, hiv_test_result, hiv_test_result_date, ctc_number) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s') ON CONFLICT (id) DO UPDATE SET hiv_test_result = '%s', hiv_test_result_date = '%s', ctc_number = '%s'", baseEntityID, baseEntityID, formSubmissionId, formSubmissionId, hivTestResults, hivTestResultsDate, ctcNumber, hivTestResults, hivTestResultsDate, ctcNumber);
         updateDB(sql);
