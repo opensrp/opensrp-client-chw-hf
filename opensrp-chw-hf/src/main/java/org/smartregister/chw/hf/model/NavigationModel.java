b027e3ba9771a9a1f8802fdf7c83f1d353c3688b
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-11-21 11:22:43 +0300

    :hamer: refactored Navigation Model

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/model/NavigationModel.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/model/NavigationModel.java
@@ -38,12 +38,12 @@ public class NavigationModel implements org.smartregister.chw.core.model.Navigat
             NavigationOption op19 = new NavigationOption(R.mipmap.sidemenu_hiv, R.mipmap.sidemenu_hiv_active, R.string.menu_kvp, CoreConstants.DrawerMenu.KVP, 0);
             NavigationOption op20 = new NavigationOption(R.mipmap.sidemenu_hiv, R.mipmap.sidemenu_hiv_active, R.string.menu_prep, CoreConstants.DrawerMenu.PrEP, 0);
 
-           // ANC, PMTCT, LD, PNC, HEI, Child,LTFU, Referrals
+            // ANC, PMTCT, LD, PNC, HEI, Child,LTFU, Referrals
             if (BuildConfig.BUILD_FOR_BORESHA_AFYA_SOUTH) {
                 if (BuildConfig.BUILD_FOR_PMTCT_CASE_BASED_MANAGEMENT) {
                     navigationOptions.addAll(Arrays.asList(op1, op3, op4, op13, op14, op12));
                 } else {
-                    navigationOptions.addAll(Arrays.asList(op1, op9, op8, op10, op3, op13, op4, op14, op16, op12));
+                    navigationOptions.addAll(Arrays.asList(op1, op9, op8, op10, op3, op4, op13, op14, op16, op12));
                 }
                 if (HealthFacilityApplication.getApplicationFlavor().hasHivst()) {
                     navigationOptions.add(2, op17);
@@ -56,14 +56,13 @@ public class NavigationModel implements org.smartregister.chw.core.model.Navigat
                     navigationOptions.add(4, op19);
                 }
                 if (HealthFacilityApplication.getApplicationFlavor().hasLD()) {
-                    if(BuildConfig.BUILD_FOR_PMTCT_CASE_BASED_MANAGEMENT) {
+                    if (BuildConfig.BUILD_FOR_PMTCT_CASE_BASED_MANAGEMENT) {
                         navigationOptions.add(2, op15);
-                    }
-                    else{
+                    } else {
                         navigationOptions.add(6, op15);
                     }
                 }
-                if(HealthFacilityApplication.getApplicationFlavor().hasChildModule()) {
+                if (HealthFacilityApplication.getApplicationFlavor().hasChildModule()) {
                     navigationOptions.add(9, op5);
                 }
 
