667027ffd8f499657d5a4bba737901fd7cbda670
cozej4 <ilakozejumanne@gmail.com>
2023-10-27 10:28:09 +0300

    :hammer: Added ECP Register Fragment

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/FpRegisterActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/FpRegisterActivity.java
@@ -3,13 +3,23 @@ package org.smartregister.chw.hf.activity;
 import android.app.Activity;
 import android.content.Intent;
 
+import androidx.annotation.MenuRes;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+
 import org.smartregister.chw.core.activity.CoreFpRegisterActivity;
 import org.smartregister.chw.fp.model.BaseFpRegisterModel;
 import org.smartregister.chw.fp.util.FamilyPlanningConstants;
+import org.smartregister.chw.hf.R;
+import org.smartregister.chw.hf.fragment.FpEcpRegisterFragment;
 import org.smartregister.chw.hf.fragment.FpRegisterFragment;
 import org.smartregister.chw.hf.interactor.HFFamilyPlanningRegisterInteractor;
+import org.smartregister.chw.hf.listener.AncBottomNavigationListener;
+import org.smartregister.chw.hf.listener.FpBottomNavigationListener;
 import org.smartregister.chw.hf.presenter.FpRegisterPresenter;
 import org.smartregister.helper.BottomNavigationHelper;
+import org.smartregister.listener.BottomNavigationListener;
 import org.smartregister.view.fragment.BaseRegisterFragment;
 
 public class FpRegisterActivity extends CoreFpRegisterActivity {
@@ -37,10 +47,38 @@ public class FpRegisterActivity extends CoreFpRegisterActivity {
         return new FpRegisterFragment();
     }
 
+    protected Fragment[] getOtherFragments() {
+        Fragment[] fragments = new Fragment[1];
+        fragments[0] = new FpEcpRegisterFragment();
+        return fragments;
+    }
+
     @Override
     protected void registerBottomNavigation() {
+        super.registerBottomNavigation();
         bottomNavigationHelper = new BottomNavigationHelper();
-        bottomNavigationView = findViewById(org.smartregister.R.id.bottom_navigation);
-        FamilyRegisterActivity.registerBottomNavigation(bottomNavigationHelper, bottomNavigationView, this);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        if (bottomNavigationView != null) {
+            bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.R.id.action_clients);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.chw.tb.R.id.action_register);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.R.id.action_search);
+            bottomNavigationView.getMenu().removeItem(org.smartregister.R.id.action_library);
+            bottomNavigationView.getMenu().removeItem(R.id.action_scan_qr);
+            BottomNavigationListener fpBottomNavigationListener = getBottomNavigation(this);
+            bottomNavigationView.setOnNavigationItemSelectedListener(fpBottomNavigationListener);
+
+        }
     }
+
+    public BottomNavigationListener getBottomNavigation(Activity activity) {
+        return new FpBottomNavigationListener(activity);
+    }
+
+    @MenuRes
+    public int getMenuResource() {
+        return R.menu.bottom_nav_fp_menu;
+    }
+
 }
